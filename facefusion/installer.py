c254ea6923a64164822cd235f65d8a68140b77de
henryruhs <info@henryruhs.com>
2023-08-27 00:10:24 +0200

    Improve installer quite a bit

--- facefusion/installer.py
+++ facefusion/installer.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Tuple
 import os
 import platform
 import shutil
@@ -11,52 +11,55 @@ import inquirer
 
 from facefusion import wording
 
-ONNXRUNTIMES : Dict[str, str] =\
+ONNXRUNTIMES : Dict[str, Tuple[str, str]] =\
 {
-	'cpu': 'onnxruntime==1.15.1',
-	'cuda': 'onnxruntime-gpu==1.15.1',
-	'coreml-legacy': 'onnxruntime-coreml==1.13.1',
-	'coreml-silicon': 'onnxruntime-silicon==1.13.1',
-	'directml': 'onnxruntime-directml==1.15.1',
-	'openvino': 'onnxruntime-openvino==1.15.0'
+	'cpu': ('onnxruntime', '1.15.1'),
+	'cuda': ('onnxruntime-gpu', '1.15.1'),
+	'coreml-legacy': ('onnxruntime-coreml', '1.13.1'),
+	'coreml-silicon': ('onnxruntime-silicon', '1.13.1'),
+	'directml': ('onnxruntime-directml', '1.15.1'),
+	'openvino': ('onnxruntime-openvino', '1.15.0')
 }
 
 
 def run() -> None:
 	virtual_environment = None
+	onnxruntime_key = None
 	onnxruntime_name = None
+	onnxruntime_version = None
 	answers : Dict[str, str] = inquirer.prompt(
 	[
 		inquirer.List(
 			'virtual_environment',
 			message = wording.get('select_virtual_environment_install'),
-			choices = [ 'conda', 'venv', 'none' ],
+			choices = [ 'conda', 'venv', 'none' ]
 		),
 		inquirer.List(
 			'onnxruntime_key',
 			message = wording.get('select_onnxruntime_install'),
-			choices = list(ONNXRUNTIMES.keys()),
+			choices = list(ONNXRUNTIMES.keys())
 		)
 	])
 
 	if answers is not None:
 		virtual_environment = answers['virtual_environment']
 		onnxruntime_key = answers['onnxruntime_key']
-		onnxruntime_name = ONNXRUNTIMES[onnxruntime_key]
+		onnxruntime_name, onnxruntime_version = ONNXRUNTIMES[onnxruntime_key]
 		shutil.rmtree('venv', ignore_errors = True)
 	if virtual_environment == 'conda':
-		subprocess.call([ 'conda', 'create', '--prefix', 'venv', '-y' ])
+		subprocess.call([ 'conda', 'create', '--name', 'venv', '-y' ])
+		subprocess.call([ 'conda', 'activate', 'venv' ])
 	if virtual_environment == 'venv':
-		subprocess.run([ sys.executable, '-m', 'venv', 'venv' ])
+		subprocess.call([ sys.executable, '-m', 'venv', 'venv' ])
 		if platform.system().lower() == 'windows':
 			activate_path = os.path.join('venv', 'Scripts', 'activate.bat')
-			subprocess.run([ activate_path ])
+			subprocess.call([ activate_path ])
 		else:
 			activate_path = os.path.join('venv', 'bin', 'activate')
-			subprocess.run([ 'source', activate_path ], shell = True)
+			subprocess.call([ 'bash', '-c', 'source ' + activate_path ])
 	if answers is not None:
 		subprocess.call([ 'pip', 'install', '-r', 'requirements.txt' ])
-	if onnxruntime_name:
-		if onnxruntime_name != 'cpu':
+	if onnxruntime_key:
+		if onnxruntime_key != 'cpu':
 			subprocess.call([ 'pip', 'uninstall', 'onnxruntime', onnxruntime_name, '-y' ])
-		subprocess.call([ 'pip', 'install', onnxruntime_name ])
+		subprocess.call([ 'pip', 'install', onnxruntime_name + '==' + onnxruntime_version ])
