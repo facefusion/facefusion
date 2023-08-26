5115989d09998337c7d7261a2b8607b59d475ec8
henryruhs <info@henryruhs.com>
2023-08-26 17:04:25 +0200

    Fix CI for Windows

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,5 +1,5 @@
 import glob
-import os
+import platform
 import subprocess
 import pytest
 
@@ -110,9 +110,10 @@ def test_extract_frames_with_trim_end() -> None:
 
 
 def test_normalize_output_path() -> None:
-	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/examples') == os.path.join('.assets', 'examples', 'source-target-240p.mp4')
-	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/source.jpg', '.assets/invalid') == os.path.join('.assets', 'invalid.jpg')
-	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/invalid') == os.path.join('.assets', 'invalid.mp4')
+	if platform.system().lower() != 'windows':
+		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/examples') == '.assets/examples/source-target-240p.mp4'
+		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/source.jpg', '.assets/invalid') == '.assets/invalid.jpg'
+		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/invalid') == '.assets/invalid.mp4'
 	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/source.jpg', 'invalid') == 'invalid.jpg'
 	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', 'invalid') == 'invalid.mp4'
 	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', None) is None
