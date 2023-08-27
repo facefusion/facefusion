8a4270b88c2ef1e61e18cb07be3fe78764f84d64
henryruhs <info@henryruhs.com>
2023-08-27 22:12:54 +0200

    Refine the output path normalization

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -112,10 +112,10 @@ def test_extract_frames_with_trim_end() -> None:
 def test_normalize_output_path() -> None:
 	if platform.system().lower() != 'windows':
 		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/examples') == '.assets/examples/source-target-240p.mp4'
-		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/examples/valid.mp4') == '.assets/examples/valid.mp4'
-		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/invalid.mov') == '.assets/invalid.mp4'
+		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/examples/output.mp4') == '.assets/examples/output.mp4'
+		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/output.mov') == '.assets/output.mp4'
 	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/examples/invalid') is None
-	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/invalid/valid.mp4') is None
+	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/invalid/output.mp4') is None
 	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', 'invalid') is None
 	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', None) is None
 
