8dffc342c47a3b6fba5f10bcb7effc0f7fe0daa8
henryruhs <info@henryruhs.com>
2023-08-29 17:00:32 +0200

    None for those tests that don't take source path into account

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -114,12 +114,11 @@ def test_normalize_output_path() -> None:
 		assert normalize_output_path('.assets/examples/source.jpg', None, '.assets/examples/target-240p.mp4') == '.assets/examples/target-240p.mp4'
 		assert normalize_output_path(None, '.assets/examples/target-240p.mp4', '.assets/examples/target-240p.mp4') == '.assets/examples/target-240p.mp4'
 		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/examples') == '.assets/examples/source-target-240p.mp4'
-		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/examples/output.mp4') == '.assets/examples/output.mp4'
-		assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/output.mov') == '.assets/output.mp4'
+		assert normalize_output_path(None, '.assets/examples/target-240p.mp4', '.assets/examples/output.mp4') == '.assets/examples/output.mp4'
+		assert normalize_output_path(None, '.assets/examples/target-240p.mp4', '.assets/output.mov') == '.assets/output.mp4'
 	assert normalize_output_path(None, '.assets/examples/target-240p.mp4', '.assets/examples/invalid') is None
-	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/examples/invalid') is None
-	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', '.assets/invalid/output.mp4') is None
-	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', 'invalid') is None
+	assert normalize_output_path(None, '.assets/examples/target-240p.mp4', '.assets/invalid/output.mp4') is None
+	assert normalize_output_path(None, '.assets/examples/target-240p.mp4', 'invalid') is None
 	assert normalize_output_path('.assets/examples/source.jpg', '.assets/examples/target-240p.mp4', None) is None
 
 
