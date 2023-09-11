cc8169755aad881e0bd3767a597b85c37ecb56e7
henryruhs <info@henryruhs.com>
2023-09-11 11:53:16 +0200

    Respect trim on videos with audio (finally)

--- facefusion/utilities.py
+++ facefusion/utilities.py
@@ -17,7 +17,8 @@ from facefusion import wording
 from facefusion.vision import detect_fps
 
 TEMP_DIRECTORY_PATH = os.path.join(tempfile.gettempdir(), 'facefusion')
-TEMP_OUTPUT_NAME = 'temp.mp4'
+TEMP_OUTPUT_VIDEO_NAME = 'temp.mp4'
+TEMP_OUTPUT_AUDIO_NAME = 'temp.mp3'
 
 # monkey patch ssl
 if platform.system().lower() == 'darwin':
@@ -41,11 +42,11 @@ def open_ffmpeg(args : List[str]) -> subprocess.Popen[bytes]:
 
 
 def extract_frames(target_path : str, fps : float) -> bool:
-	temp_directory_path = get_temp_directory_path(target_path)
 	temp_frame_compression = round(31 - (facefusion.globals.temp_frame_quality * 0.31))
 	trim_frame_start = facefusion.globals.trim_frame_start
 	trim_frame_end = facefusion.globals.trim_frame_end
-	commands = [ '-hwaccel', 'auto', '-i', target_path, '-q:v', str(temp_frame_compression), '-pix_fmt', 'rgb24', '-map', '0:v:0' ]
+	temp_frames_pattern = get_temp_frames_pattern(target_path)
+	commands = [ '-hwaccel', 'auto', '-i', target_path, '-q:v', str(temp_frame_compression), '-pix_fmt', 'rgb24' ]
 	if trim_frame_start is not None and trim_frame_end is not None:
 		commands.extend([ '-vf', 'trim=start_frame=' + str(trim_frame_start) + ':end_frame=' + str(trim_frame_end) + ',fps=' + str(fps) ])
 	elif trim_frame_start is not None:
@@ -54,7 +55,7 @@ def extract_frames(target_path : str, fps : float) -> bool:
 		commands.extend([ '-vf', 'trim=end_frame=' + str(trim_frame_end) + ',fps=' + str(fps) ])
 	else:
 		commands.extend([ '-vf', 'fps=' + str(fps) ])
-	commands.extend([ os.path.join(temp_directory_path, '%04d.' + facefusion.globals.temp_frame_format) ])
+	commands.extend([ '-vsync', '0', temp_frames_pattern ])
 	return run_ffmpeg(commands)
 
 
@@ -65,19 +66,19 @@ def compress_image(output_path : str) -> bool:
 
 
 def merge_video(target_path : str, fps : float) -> bool:
-	temp_output_path = get_temp_output_path(target_path)
-	temp_directory_path = get_temp_directory_path(target_path)
-	commands = [ '-hwaccel', 'auto', '-r', str(fps), '-i', os.path.join(temp_directory_path, '%04d.' + facefusion.globals.temp_frame_format), '-c:v', facefusion.globals.output_video_encoder ]
+	temp_output_video_path = get_temp_output_video_path(target_path)
+	temp_frames_pattern = get_temp_frames_pattern(target_path)
+	commands = [ '-hwaccel', 'auto', '-r', str(fps), '-i', temp_frames_pattern, '-c:v', facefusion.globals.output_video_encoder ]
 	if facefusion.globals.output_video_encoder in [ 'libx264', 'libx265' ]:
 		output_video_compression = round(51 - (facefusion.globals.output_video_quality * 0.5))
 		commands.extend([ '-crf', str(output_video_compression) ])
-	if facefusion.globals.output_video_encoder in [ 'libvpx' ]:
+	if facefusion.globals.output_video_encoder in [ 'libvpx-vp9' ]:
 		output_video_compression = round(63 - (facefusion.globals.output_video_quality * 0.5))
 		commands.extend([ '-crf', str(output_video_compression) ])
 	if facefusion.globals.output_video_encoder in [ 'h264_nvenc', 'hevc_nvenc' ]:
 		output_video_compression = round(51 - (facefusion.globals.output_video_quality * 0.5))
 		commands.extend([ '-cq', str(output_video_compression) ])
-	commands.extend([ '-pix_fmt', 'yuv420p', '-vf', 'colorspace=bt709:iall=bt601-6-625', '-y', temp_output_path ])
+	commands.extend([ '-pix_fmt', 'yuv420p', '-y', temp_output_video_path ])
 	return run_ffmpeg(commands)
 
 
@@ -85,22 +86,18 @@ def restore_audio(target_path : str, output_path : str) -> bool:
 	fps = detect_fps(target_path)
 	trim_frame_start = facefusion.globals.trim_frame_start
 	trim_frame_end = facefusion.globals.trim_frame_end
-	temp_output_path = get_temp_output_path(target_path)
-	commands = [ '-hwaccel', 'auto', '-i', temp_output_path, '-i', target_path ]
-	if trim_frame_start is None and trim_frame_end is None:
-		commands.extend([ '-c:a', 'copy' ])
-	else:
-		if trim_frame_start is not None:
-			start_time = trim_frame_start / fps
-			commands.extend([ '-ss', str(start_time) ])
-		else:
-			commands.extend([ '-ss', '0' ])
-		if trim_frame_end is not None:
-			end_time = trim_frame_end / fps
-			commands.extend([ '-to', str(end_time) ])
-		commands.extend([ '-c:a', 'aac' ])
-	commands.extend([ '-map', '0:v:0', '-map', '1:a:0', '-y', output_path ])
-	return run_ffmpeg(commands)
+	temp_output_video_path = get_temp_output_video_path(target_path)
+	temp_output_audio_path = get_temp_output_audio_path(target_path)
+	extract_commands = [ '-hwaccel', 'auto', '-i', target_path ]
+	if trim_frame_start is not None:
+		start_time = trim_frame_start / fps
+		extract_commands.extend([ '-ss', str(start_time) ])
+	if trim_frame_end is not None:
+		end_time = trim_frame_end / fps
+		extract_commands.extend([ '-to', str(end_time) ])
+	extract_commands.extend([ '-vn', '-y', temp_output_audio_path ])
+	merge_commands = [ '-hwaccel', 'auto', '-i', temp_output_video_path, '-i', temp_output_audio_path, '-y', output_path ]
+	return run_ffmpeg(extract_commands) and run_ffmpeg(merge_commands)
 
 
 def get_temp_frame_paths(target_path : str) -> List[str]:
@@ -108,14 +105,24 @@ def get_temp_frame_paths(target_path : str) -> List[str]:
 	return glob.glob((os.path.join(glob.escape(temp_directory_path), '*.' + facefusion.globals.temp_frame_format)))
 
 
+def get_temp_frames_pattern(target_path : str) -> str:
+	temp_directory_path = get_temp_directory_path(target_path)
+	return os.path.join(temp_directory_path, '%04d.' + facefusion.globals.temp_frame_format)
+
+
 def get_temp_directory_path(target_path : str) -> str:
 	target_name, _ = os.path.splitext(os.path.basename(target_path))
 	return os.path.join(TEMP_DIRECTORY_PATH, target_name)
 
 
-def get_temp_output_path(target_path : str) -> str:
+def get_temp_output_video_path(target_path : str) -> str:
 	temp_directory_path = get_temp_directory_path(target_path)
-	return os.path.join(temp_directory_path, TEMP_OUTPUT_NAME)
+	return os.path.join(temp_directory_path, TEMP_OUTPUT_VIDEO_NAME)
+
+
+def get_temp_output_audio_path(target_path : str) -> str:
+	temp_directory_path = get_temp_directory_path(target_path)
+	return os.path.join(temp_directory_path, TEMP_OUTPUT_AUDIO_NAME)
 
 
 def normalize_output_path(source_path : Optional[str], target_path : Optional[str], output_path : Optional[str]) -> Optional[str]:
@@ -139,11 +146,11 @@ def create_temp(target_path : str) -> None:
 
 
 def move_temp(target_path : str, output_path : str) -> None:
-	temp_output_path = get_temp_output_path(target_path)
-	if is_file(temp_output_path):
+	temp_output_video_path = get_temp_output_video_path(target_path)
+	if is_file(temp_output_video_path):
 		if is_file(output_path):
 			os.remove(output_path)
-		shutil.move(temp_output_path, output_path)
+		shutil.move(temp_output_video_path, output_path)
 
 
 def clear_temp(target_path : str) -> None:
