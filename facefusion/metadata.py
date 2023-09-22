7c4de55c55ef20e3724396ee8635947de2000625
henryruhs <info@henryruhs.com>
2023-09-22 10:19:44 +0200

    Looks stable to me

--- facefusion/metadata.py
+++ facefusion/metadata.py
@@ -2,7 +2,7 @@ METADATA =\
 {
 	'name': 'FaceFusion',
 	'description': 'Next generation face swapper and enhancer',
-	'version': 'NEXT',
+	'version': '1.2.1',
 	'license': 'MIT',
 	'author': 'Henry Ruhs',
 	'url': 'https://facefusion.io'
