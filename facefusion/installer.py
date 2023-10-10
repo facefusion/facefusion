5708be37a2a9139e6012c05d1b2775909c7e8039
henryruhs <info@henryruhs.com>
2023-10-10 12:38:09 +0200

    Okay, here we have default torch

--- facefusion/installer.py
+++ facefusion/installer.py
@@ -10,9 +10,10 @@ from facefusion import metadata, wording
 
 TORCH : Dict[str, str] =\
 {
-	'default': 'https://download.pytorch.org/whl/cpu',
-	'cuda': 'https://download.pytorch.org/whl/cu118',
-	'rocm': 'https://download.pytorch.org/whl/rocm5.6'
+	'default': 'default',
+	'cpu': 'cpu',
+	'cuda': 'cu118',
+	'rocm': 'rocm5.6'
 }
 ONNXRUNTIMES : Dict[str, Tuple[str, str]] =\
 {
@@ -58,10 +59,13 @@ def run(program : ArgumentParser) -> None:
 		])
 	if answers is not None:
 		torch = answers['torch']
-		torch_url = TORCH[torch]
+		torch_wheel = TORCH[torch]
 		onnxruntime = answers['onnxruntime']
 		onnxruntime_name, onnxruntime_version = ONNXRUNTIMES[onnxruntime]
 		subprocess.call([ 'pip', 'uninstall', 'torch', '-y' ])
-		subprocess.call([ 'pip', 'install', '-r', 'requirements.txt', '--extra-index-url', torch_url ])
+		if torch_wheel == 'default':
+			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt' ])
+		else:
+			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt', '--extra-index-url', 'https://download.pytorch.org/whl/' + torch_wheel ])
 		subprocess.call([ 'pip', 'uninstall', 'onnxruntime', onnxruntime_name, '-y' ])
 		subprocess.call([ 'pip', 'install', onnxruntime_name + '==' + onnxruntime_version ])
