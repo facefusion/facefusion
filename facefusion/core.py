857bede91e454d2ffa6a6adf8ad76ba8cfaacd59
henryruhs <info@henryruhs.com>
2023-10-30 22:17:45 +0100

    Disable onnxruntime warnings

--- facefusion/core.py
+++ facefusion/core.py
@@ -1,7 +1,6 @@
 import os
 
 os.environ['OMP_NUM_THREADS'] = '1'
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import signal
 import sys
@@ -18,6 +17,7 @@ from facefusion.predictor import predict_image, predict_video
 from facefusion.processors.frame.core import get_frame_processors_modules, load_frame_processor_module
 from facefusion.utilities import is_image, is_video, detect_fps, compress_image, merge_video, extract_frames, get_temp_frame_paths, restore_audio, create_temp, move_temp, clear_temp, list_module_names, encode_execution_providers, decode_execution_providers, normalize_output_path, create_metavar, update_status
 
+onnxruntime.set_default_logger_severity(3)
 warnings.filterwarnings('ignore', category = UserWarning, module = 'torchvision')
 
 
