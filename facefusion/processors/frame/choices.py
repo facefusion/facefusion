f5b946c0144d697850af29efdadcb1194d32c1dd
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2023-11-17 18:49:45 +0530

    implement blendface

--- facefusion/processors/frame/choices.py
+++ facefusion/processors/frame/choices.py
@@ -2,7 +2,7 @@ from typing import List
 
 import numpy
 
-face_swapper_models : List[str] = [ 'inswapper_128', 'inswapper_128_fp16', 'simswap_256', 'simswap_512_unofficial' ]
+face_swapper_models : List[str] = [ 'inswapper_128', 'inswapper_128_fp16', 'simswap_256', 'simswap_512_unofficial', 'blendface_256' ]
 face_enhancer_models : List[str] = [ 'codeformer', 'gfpgan_1.2', 'gfpgan_1.3', 'gfpgan_1.4', 'gpen_bfr_512', 'restoreformer' ]
 frame_enhancer_models : List[str] = [ 'real_esrgan_x2plus', 'real_esrgan_x4plus', 'real_esrnet_x4plus' ]
 
