ecb87ca578274ba1fb4eda390507901715dc9e60
henryruhs <info@henryruhs.com>
2023-11-24 21:13:18 +0100

    Gracefully start and stop webcam

--- facefusion/uis/components/webcam.py
+++ facefusion/uis/components/webcam.py
@@ -19,9 +19,35 @@ from facefusion.vision import normalize_frame_color, read_static_image
 from facefusion.uis.typing import StreamMode, WebcamMode
 from facefusion.uis.core import get_ui_component
 
+WEBCAM_CAPTURE : Optional[cv2.VideoCapture] = None
 WEBCAM_IMAGE : Optional[gradio.Image] = None
 WEBCAM_START_BUTTON : Optional[gradio.Button] = None
 WEBCAM_STOP_BUTTON : Optional[gradio.Button] = None
+
+
+def get_webcam_capture(resolution : str, fps : float) -> Optional[cv2.VideoCapture]:
+	global WEBCAM_CAPTURE
+
+	if WEBCAM_CAPTURE is None:
+		if platform.system().lower() == 'windows':
+			WEBCAM_CAPTURE = cv2.VideoCapture(-1, cv2.CAP_DSHOW)
+		else:
+			WEBCAM_CAPTURE = cv2.VideoCapture(-1)
+		if WEBCAM_CAPTURE.isOpened():
+			webcam_width, webcam_height = map(int, resolution.split('x'))
+			WEBCAM_CAPTURE.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))  # type: ignore[attr-defined]
+			WEBCAM_CAPTURE.set(cv2.CAP_PROP_FRAME_WIDTH, webcam_width)
+			WEBCAM_CAPTURE.set(cv2.CAP_PROP_FRAME_HEIGHT, webcam_height)
+			WEBCAM_CAPTURE.set(cv2.CAP_PROP_FPS, fps)
+	return WEBCAM_CAPTURE
+
+
+def clear_webcam_capture() -> None:
+	global WEBCAM_CAPTURE
+
+	if WEBCAM_CAPTURE:
+		WEBCAM_CAPTURE.release()
+	WEBCAM_CAPTURE = None
 
 
 def render() -> None:
@@ -63,9 +89,9 @@ def start(mode : WebcamMode, resolution : str, fps : float) -> Generator[Frame, 
 	stream = None
 	if mode in [ 'udp', 'v4l2' ]:
 		stream = open_stream(mode, resolution, fps) # type: ignore[arg-type]
-	capture = capture_webcam(resolution, fps)
-	if capture.isOpened():
-		for capture_frame in multi_process_capture(source_face, capture, fps):
+	webcam_capture = get_webcam_capture(resolution, fps)
+	if webcam_capture.isOpened():
+		for capture_frame in multi_process_capture(source_face, webcam_capture, fps):
 			if mode == 'inline':
 				yield normalize_frame_color(capture_frame)
 			else:
@@ -73,13 +99,13 @@ def start(mode : WebcamMode, resolution : str, fps : float) -> Generator[Frame, 
 				yield None
 
 
-def multi_process_capture(source_face : Face, capture : cv2.VideoCapture, fps : float) -> Generator[Frame, None, None]:
+def multi_process_capture(source_face : Face, webcam_capture : cv2.VideoCapture, fps : float) -> Generator[Frame, None, None]:
 	progress = tqdm(desc = wording.get('processing'), unit = 'frame')
 	with ThreadPoolExecutor(max_workers = facefusion.globals.execution_thread_count) as executor:
 		futures = []
 		deque_capture_frames : Deque[Frame] = deque()
-		while capture.isOpened():
-			_, capture_frame = capture.read()
+		while webcam_capture.isOpened():
+			_, capture_frame = webcam_capture.read()
 			if analyse_stream(capture_frame, fps):
 				return
 			future = executor.submit(process_stream_frame, source_face, capture_frame)
@@ -94,20 +120,8 @@ def multi_process_capture(source_face : Face, capture : cv2.VideoCapture, fps : 
 
 
 def stop() -> gradio.Image:
+	clear_webcam_capture()
 	return gradio.Image(value = None)
-
-
-def capture_webcam(resolution : str, fps : float) -> cv2.VideoCapture:
-	webcam_width, webcam_height = map(int, resolution.split('x'))
-	if platform.system().lower() == 'windows':
-		capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-	else:
-		capture = cv2.VideoCapture(0)
-	capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG')) # type: ignore[attr-defined]
-	capture.set(cv2.CAP_PROP_FRAME_WIDTH, webcam_width)
-	capture.set(cv2.CAP_PROP_FRAME_HEIGHT, webcam_height)
-	capture.set(cv2.CAP_PROP_FPS, fps)
-	return capture
 
 
 def process_stream_frame(source_face : Face, temp_frame : Frame) -> Frame:
