3e93f99eeb3f438dc416b1d82d91db742e791442
henryruhs <info@henryruhs.com>
2023-12-26 19:41:49 +0100

    Bump version

--- facefusion/metadata.py
+++ facefusion/metadata.py
@@ -2,7 +2,7 @@ METADATA =\
 {
 	'name': 'FaceFusion',
 	'description': 'Next generation face swapper and enhancer',
-	'version': '2.1.2',
+	'version': '2.1.3',
 	'license': 'MIT',
 	'author': 'Henry Ruhs',
 	'url': 'https://facefusion.io'
