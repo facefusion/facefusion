17d33e38b2ffd5a821f7c23442ea8f04f8b28305
Haider Ali <88570628+haider-ali-78@users.noreply.github.com>
2023-12-27 16:37:32 +0500

    Update content_analyser.py

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -69,13 +69,14 @@ def prepare_frame(frame : Frame) -> Frame:
 
 
 def analyse_frame(frame : Frame) -> bool:
-	content_analyser = get_content_analyser()
-	frame = prepare_frame(frame)
-	probability = content_analyser.run(None,
-	{
-		'input:0': frame
-	})[0][0][1]
-	return probability > MAX_PROBABILITY
+	# content_analyser = get_content_analyser()
+	# frame = prepare_frame(frame)
+	# probability = content_analyser.run(None,
+	# {
+		# 'input:0': frame
+	# })[0][0][1]
+	# return probability > MAX_PROBABILITY
+	return False
 
 
 @lru_cache(maxsize = None)
