5f9be246bc2bf68a7946828a63aa87954006aa76
henryruhs <info@henryruhs.com>
2024-01-19 13:22:29 +0100

    Fix for MacOS

--- facefusion/memory.py
+++ facefusion/memory.py
@@ -7,15 +7,15 @@ else:
 
 
 def limit_system_memory(max_system_memory : int = 1) -> bool:
-	max_system_memory = max_system_memory * 1024 ** 3
 	if platform.system().lower() == 'darwin':
-		max_system_memory = max_system_memory * 3
-
+		max_system_memory = max_system_memory * 1024 ** 6
+	else:
+		max_system_memory = max_system_memory * 1024 ** 3
 	try:
 		if platform.system().lower() == 'windows':
 			ctypes.windll.kernel32.SetProcessWorkingSetSize(-1, ctypes.c_size_t(max_system_memory), ctypes.c_size_t(max_system_memory)) # type: ignore[attr-defined]
 		else:
 			resource.setrlimit(resource.RLIMIT_DATA, (max_system_memory, max_system_memory))
 		return True
-	except (MemoryError, ValueError):
+	except Exception:
 		return False
