32d2941f32f158a426db0786515614bf1161892b
henryruhs <info@henryruhs.com>
2024-01-28 20:40:53 +0100

    Revert venv on the fly

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
-import subprocess
+from facefusion import core
 
 if __name__ == '__main__':
-	if 'venv' in sys.executable or '--skip-venv' in sys.argv:
-		from facefusion import core
-
-		core.cli()
-	else:
-		try:
-			subprocess.run([ 'venv/bin/python', 'run.py' ])
-		except:
-			sys.exit()
+	core.cli()
