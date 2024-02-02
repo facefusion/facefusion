595a7ffb3bc5f87a94b3ea1eb28c3caf593718cb
henryruhs <info@henryruhs.com>
2024-02-02 23:53:29 +0100

    Remove face debugger test for now

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,22 +16,6 @@ def before_all() -> None:
 	subprocess.run([ 'ffmpeg', '-i', '.assets/examples/target-240p.mp4', '-vframes', '1', '.assets/examples/target-240p.jpg' ])
 
 
-def test_debug_face_to_image() -> None:
-	commands = [ sys.executable, 'run.py', '--frame-processors', 'face_debugger', '-t', '.assets/examples/target-240p.jpg', '-o', '.assets/examples', '--headless' ]
-	run = subprocess.run(commands, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-
-	assert run.returncode == 0
-	assert 'image succeed' in run.stdout.decode()
-
-
-def test_debug_face_to_video() -> None:
-	commands = [ sys.executable, 'run.py', '--frame-processors', 'face_debugger', '-t', '.assets/examples/target-240p.mp4', '-o', '.assets/examples', '--trim-frame-end', '10', '--headless' ]
-	run = subprocess.run(commands, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-
-	assert run.returncode == 0
-	assert 'video succeed' in run.stdout.decode()
-
-
 def test_enhance_face_to_image() -> None:
 	commands = [ sys.executable, 'run.py', '--frame-processors', 'face_enhancer', '-t', '.assets/examples/target-240p.jpg', '-o', '.assets/examples', '--headless' ]
 	run = subprocess.run(commands, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
