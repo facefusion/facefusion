ff97917feb0ad796bbbce0caba4ab94d5362f6b2
henryruhs <info@henryruhs.com>
2024-02-22 18:01:07 +0100

    Minor naming

--- facefusion/face_helper.py
+++ facefusion/face_helper.py
@@ -116,9 +116,9 @@ def convert_face_landmark_68_to_5(landmark_68 : FaceLandmark68) -> FaceLandmark5
 	left_eye = numpy.mean(landmark_68[36:42], axis = 0)
 	right_eye = numpy.mean(landmark_68[42:48], axis = 0)
 	nose = landmark_68[30]
-	left_mouth_end = landmark_68[48]
-	right_mouth_end = landmark_68[54]
-	face_landmark_5 = numpy.array([ left_eye, right_eye, nose, left_mouth_end, right_mouth_end ])
+	left_mouth = landmark_68[48]
+	right_mouth = landmark_68[54]
+	face_landmark_5 = numpy.array([ left_eye, right_eye, nose, left_mouth, right_mouth ])
 	return face_landmark_5
 
 
