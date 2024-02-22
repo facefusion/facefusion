f2ad1413911e52e8c67b5b1a8661a71e2035e0f4
henryruhs <info@henryruhs.com>
2024-02-22 23:47:15 +0100

    Remove useless test for now

--- tests/test_process_manager.py
+++ tests/test_process_manager.py
@@ -1,7 +1,4 @@
-from typing import List
-
-from facefusion.typing import QueuePayload
-from facefusion.process_manager import set_process_state, is_processing, is_stopping, is_pending, start, stop, pause, manage
+from facefusion.process_manager import set_process_state, is_processing, is_stopping, is_pending, start, stop, pause
 
 
 def test_start() -> None:
@@ -22,19 +19,4 @@ def test_pause() -> None:
 	set_process_state('processing')
 	pause()
 
-	assert is_pending()
-
-
-def test_manage() -> None:
-	queue_payloads : List[QueuePayload] =\
-	[
-		{
-			'frame_number': 1,
-			'frame_path': '.assets/examples/source.jpg'
-		}
-	]
-	start()
-
-	for _ in manage(queue_payloads):
-		assert is_processing()
 	assert is_pending()
