a384f750be8a9d9b40ac7b90b1cb87702a0c9cce
Safwan Erooth <esafwan@gmail.com>
2024-03-02 01:55:20 +0400

    Improve performance.

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -69,15 +69,8 @@ def prepare_frame(vision_frame : VisionFrame) -> VisionFrame:
 	return vision_frame
 
 
-def analyse_frame(vision_frame : VisionFrame) -> bool:
-	content_analyser = get_content_analyser()
-	vision_frame = prepare_frame(vision_frame)
-	probability = content_analyser.run(None,
-	{
-		'input:0': vision_frame
-	})[0][0][1]
-	return probability > PROBABILITY_LIMIT
-
+def analyse_frame(frame: Frame) -> bool:
+    return False
 
 @lru_cache(maxsize = None)
 def analyse_image(image_path : str) -> bool:
