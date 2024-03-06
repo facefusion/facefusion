18d0ece197aca1370b958e24786e1387444bed57
henryruhs <info@henryruhs.com>
2024-03-06 18:42:56 +0100

    Undo lowercase of values

--- facefusion/execution.py
+++ facefusion/execution.py
@@ -34,7 +34,7 @@ def apply_execution_provider_options(execution_providers: List[str]) -> List[Any
 
 def use_exhaustive() -> bool:
 	cuda_devices = detect_static_cuda_devices()
-	product_names = [ 'geforce gtx 1650', 'geforce gtx 1660' ]
+	product_names = [ 'GeForce GTX 1650', 'GeForce GTX 1660' ]
 
 	return any(cuda_device.get('product').get('name') in product_names for cuda_device in cuda_devices)
 
@@ -53,34 +53,34 @@ def detect_cuda_devices() -> List[ExecutionDevice]:
 	cuda_devices : List[ExecutionDevice] = []
 	try:
 		output, _ = run_nvidia_smi().communicate()
-		contents = ElementTree.fromstring(output)
+		root_element = ElementTree.fromstring(output)
 	except FileNotFoundError:
-		contents = ElementTree.Element('xml')
+		root_element = ElementTree.Element('xml')
 
-	for gpu in contents.findall('gpu'):
+	for gpu_element in root_element.findall('gpu'):
 		cuda_devices.append(
 		{
-			'driver_version': contents.find('.//driver_version').text,
+			'driver_version': root_element.find('.//driver_version').text,
 			'framework':
 			{
 				'name': 'cuda',
-				'version': contents.find('.//cuda_version').text,
+				'version': root_element.find('.//cuda_version').text,
 			},
 			'product':
 			{
-				'vendor': 'nvidia',
-				'name': gpu.find('product_name').text.lower().replace('nvidia ', ''),
-				'architecture': gpu.find('product_architecture').text.lower(),
+				'vendor': 'NVIDIA',
+				'name': gpu_element.find('product_name').text.replace('NVIDIA ', ''),
+				'architecture': gpu_element.find('product_architecture').text,
 			},
 			'video_memory':
 			{
-				'total': create_value_and_unit(gpu.find('fb_memory_usage/total').text),
-				'free': create_value_and_unit(gpu.find('fb_memory_usage/free').text)
+				'total': create_value_and_unit(gpu_element.find('fb_memory_usage/total').text),
+				'free': create_value_and_unit(gpu_element.find('fb_memory_usage/free').text)
 			},
 			'utilization':
 			{
-				'gpu': create_value_and_unit(gpu.find('utilization/gpu_util').text),
-				'memory': create_value_and_unit(gpu.find('utilization/memory_util').text)
+				'gpu': create_value_and_unit(gpu_element.find('utilization/gpu_util').text),
+				'memory': create_value_and_unit(gpu_element.find('utilization/memory_util').text)
 			}
 		})
 	return cuda_devices
