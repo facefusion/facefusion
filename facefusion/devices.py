7ea14afaf96762907ccf599f01f08f61bcfe940e
henryruhs <info@henryruhs.com>
2024-03-06 16:01:25 +0100

    Move logic to run_nvidia_smi()

--- facefusion/devices.py
+++ facefusion/devices.py
@@ -13,40 +13,44 @@ def detect_static_cuda_devices() -> List[GpuDevice]:
 
 def detect_cuda_devices() -> List[GpuDevice]:
 	cuda_devices : List[GpuDevice] = []
-
 	try:
-		output = subprocess.check_output([ 'nvidia-smi', '--query', '--xml-format' ])
-		content = ElementTree.fromstring(output)
+		output, _ = run_nvidia_smi().communicate()
+		contents = ElementTree.fromstring(output)
+	except FileNotFoundError:
+		contents = ElementTree.Element('xml')
 
-		for gpu in content.findall('gpu'):
-			cuda_devices.append(
+	for gpu in contents.findall('gpu'):
+		cuda_devices.append(
+		{
+			'driver_version': contents.find('.//driver_version').text,
+			'framework':
 			{
-				'driver_version': content.find('.//driver_version').text,
-				'framework':
-				{
-					'name': 'cuda',
-					'version': content.find('.//cuda_version').text,
-				},
-				'product':
-				{
-					'vendor': 'nvidia',
-					'name': gpu.find('product_name').text.lower().replace('nvidia ', ''),
-					'architecture': gpu.find('product_architecture').text.lower(),
-				},
-				'video_memory':
-				{
-					'total': create_value_and_unit(gpu.find('fb_memory_usage/total').text),
-					'free': create_value_and_unit(gpu.find('fb_memory_usage/free').text)
-				},
-				'utilization':
-				{
-					'gpu': create_value_and_unit(gpu.find('utilization/gpu_util').text),
-					'memory': create_value_and_unit(gpu.find('utilization/memory_util').text)
-				}
-			})
-	except Exception:
-		pass
+				'name': 'cuda',
+				'version': contents.find('.//cuda_version').text,
+			},
+			'product':
+			{
+				'vendor': 'nvidia',
+				'name': gpu.find('product_name').text.lower().replace('nvidia ', ''),
+				'architecture': gpu.find('product_architecture').text.lower(),
+			},
+			'video_memory':
+			{
+				'total': create_value_and_unit(gpu.find('fb_memory_usage/total').text),
+				'free': create_value_and_unit(gpu.find('fb_memory_usage/free').text)
+			},
+			'utilization':
+			{
+				'gpu': create_value_and_unit(gpu.find('utilization/gpu_util').text),
+				'memory': create_value_and_unit(gpu.find('utilization/memory_util').text)
+			}
+		})
 	return cuda_devices
+
+
+def run_nvidia_smi() -> subprocess.Popen[bytes]:
+	commands = [ 'nvidia-smi', '--query', '--xml-format' ]
+	return subprocess.Popen(commands, stdout = subprocess.PIPE)
 
 
 def create_value_and_unit(text : str) -> ValueAndUnit:
