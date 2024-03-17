207e0e65dab5f3514bfbce3631fa41245b0c5ab5
henryruhs <info@henryruhs.com>
2024-03-17 20:21:33 +0100

    Tidy up monkey patch

--- facefusion/uis/overrides.py
+++ facefusion/uis/overrides.py
@@ -2,14 +2,12 @@ from typing import Any
 import cv2
 import numpy
 import base64
-from PIL import Image
 
 
 def encode_array_to_base64(array : numpy.ndarray[Any, Any]) -> str:
-    buffer = cv2.imencode(".jpg", array[:, :, ::-1])[1]
-    return "data:image/jpg;base64," + base64.b64encode(buffer.tobytes()).decode("utf-8")
+	buffer = cv2.imencode('.jpg', array[:, :, ::-1])[1]
+	return 'data:image/jpg;base64,' + base64.b64encode(buffer.tobytes()).decode('utf-8')
 
 
-def encode_pil_to_base64(pil_image : Image) -> str:
-	array = numpy.asarray(pil_image)[:, :, ::-1]
-	return encode_array_to_base64(array)
+def encode_pil_to_base64(image : Any) -> str:
+	return encode_array_to_base64(numpy.asarray(image)[:, :, ::-1])
