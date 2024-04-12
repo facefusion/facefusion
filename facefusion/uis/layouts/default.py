4eebc62b7706f89f4e33df4ee1c9d0b2396a2a4e
belingud <1170202353@qq.com>
2024-04-12 14:37:34 +0800

    custom gradio share link

--- facefusion/uis/layouts/default.py
+++ facefusion/uis/layouts/default.py
@@ -1,7 +1,12 @@
+import os
+import setuptools
 import multiprocessing
 import gradio
 
 from facefusion.uis.components import about, frame_processors, frame_processors_options, execution, execution_thread_count, execution_queue_count, memory, temp_frame, output_options, common_options, source, target, output, preview, trim_frame, face_analyser, face_selector, face_masker
+
+
+GRADIO_SHARE = setuptools.distutils.util.strtobool(os.getenv('GRADIO_SHARE'))
 
 
 def pre_check() -> bool:
@@ -77,4 +82,4 @@ def listen() -> None:
 
 def run(ui : gradio.Blocks) -> None:
 	concurrency_count = min(8, multiprocessing.cpu_count())
-	ui.queue(concurrency_count = concurrency_count).launch(show_api = False, quiet = True)
+	ui.queue(concurrency_count = concurrency_count).launch(show_api = False, quiet = True, share = GRADIO_SHARE)
