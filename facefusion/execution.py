eb259a215f3d29c109c2a376cabd8787db597714
Henry Ruhs <info@henryruhs.com>
2024-04-18 14:51:38 +0200

    Full directml support (#501)
    * Introduce conditional thread management for DML support
    
    * Finish migration to thread helpers

--- facefusion/execution.py
+++ facefusion/execution.py
@@ -11,14 +11,14 @@ def encode_execution_providers(execution_providers : List[str]) -> List[str]:
 	return [ execution_provider.replace('ExecutionProvider', '').lower() for execution_provider in execution_providers ]
 
 
-def decode_execution_providers(execution_providers: List[str]) -> List[str]:
+def decode_execution_providers(execution_providers : List[str]) -> List[str]:
 	available_execution_providers = onnxruntime.get_available_providers()
 	encoded_execution_providers = encode_execution_providers(available_execution_providers)
 
 	return [ execution_provider for execution_provider, encoded_execution_provider in zip(available_execution_providers, encoded_execution_providers) if any(execution_provider in encoded_execution_provider for execution_provider in execution_providers) ]
 
 
-def apply_execution_provider_options(execution_providers: List[str]) -> List[Any]:
+def apply_execution_provider_options(execution_providers : List[str]) -> List[Any]:
 	execution_providers_with_options : List[Any] = []
 
 	for execution_provider in execution_providers:
--- facefusion/processors/frame/modules/frame_colorizer.py
+++ facefusion/processors/frame/modules/frame_colorizer.py
@@ -1,7 +1,6 @@
 from typing import Any, List, Literal, Optional
 from argparse import ArgumentParser
 from time import sleep
-import threading
 import cv2
 import numpy
 import onnxruntime
@@ -13,6 +12,7 @@ from facefusion.face_analyser import clear_face_analyser
 from facefusion.content_analyser import clear_content_analyser
 from facefusion.execution import apply_execution_provider_options
 from facefusion.normalizer import normalize_output_path
+from facefusion.thread_helper import thread_lock, thread_semaphore
 from facefusion.typing import Face, VisionFrame, UpdateProgress, ProcessMode, ModelSet, OptionsWithModel, QueuePayload
 from facefusion.common_helper import create_metavar
 from facefusion.filesystem import is_file, resolve_relative_path, is_image, is_video
@@ -23,8 +23,6 @@ from facefusion.processors.frame import globals as frame_processors_globals
 from facefusion.processors.frame import choices as frame_processors_choices
 
 FRAME_PROCESSOR = None
-THREAD_LOCK : threading.Lock = threading.Lock()
-THREAD_SEMAPHORE : threading.Semaphore = threading.Semaphore()
 NAME = __name__.upper()
 MODELS : ModelSet =\
 {
@@ -70,7 +68,7 @@ OPTIONS : Optional[OptionsWithModel] = None
 def get_frame_processor() -> Any:
 	global FRAME_PROCESSOR
 
-	with THREAD_LOCK:
+	with thread_lock():
 		while process_manager.is_checking():
 			sleep(0.5)
 		if FRAME_PROCESSOR is None:
@@ -160,7 +158,7 @@ def post_process() -> None:
 def colorize_frame(temp_vision_frame : VisionFrame) -> VisionFrame:
 	frame_processor = get_frame_processor()
 	prepare_vision_frame = prepare_temp_frame(temp_vision_frame)
-	with THREAD_SEMAPHORE:
+	with thread_semaphore():
 		color_vision_frame = frame_processor.run(None,
 		{
 			frame_processor.get_inputs()[0].name: prepare_vision_frame
