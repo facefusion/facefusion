11bc98739160c67cc3e3252ea69485c746c8ec94
sebaxakerhtc <32651506+sebaxakerhtc@users.noreply.github.com>
2024-04-22 19:06:39 +0300

    Solve error of slider using img

--- facefusion/uis/components/preview.py
+++ facefusion/uis/components/preview.py
@@ -185,7 +185,7 @@ def update_preview_frame_slider() -> gradio.Slider:
 	if is_video(facefusion.globals.target_path):
 		video_frame_total = count_video_frame_total(facefusion.globals.target_path)
 		return gradio.Slider(maximum = video_frame_total, visible = True)
-	return gradio.Slider(value = None, maximum = None, visible = False)
+	return gradio.Slider(visible = False)
 
 
 def process_preview_frame(reference_faces : FaceSet, source_face : Face, source_audio_frame : AudioFrame, target_vision_frame : VisionFrame) -> VisionFrame:
