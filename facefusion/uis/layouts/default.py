c70be0e38078f5ea6a00a3b34fc71f8b763bace0
trinityhades <11bailess11@gmail.com>
2024-05-01 11:46:12 -0700

    fix: lint

--- facefusion/uis/layouts/default.py
+++ facefusion/uis/layouts/default.py
@@ -3,6 +3,7 @@ import gradio
 
 from facefusion.uis.components import about, frame_processors, frame_processors_options, execution, execution_thread_count, execution_queue_count, memory, temp_frame, output_options, common_options, source, target, output, preview, trim_frame, face_analyser, face_selector, face_masker, config_saving
 
+
 def pre_check() -> bool:
 	return True
 
