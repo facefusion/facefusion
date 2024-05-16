549355f88626a7d2508d989a427a57ff9369aa4d
lixiaofeng <lixiaofeng@apusapps.com>
2024-05-16 10:48:22 +0800

    support custome server ip + port

--- facefusion/uis/layouts/default.py
+++ facefusion/uis/layouts/default.py
@@ -77,4 +77,4 @@ def listen() -> None:
 
 def run(ui : gradio.Blocks) -> None:
 	concurrency_count = min(8, multiprocessing.cpu_count())
-	ui.queue(concurrency_count = concurrency_count).launch(show_api = False, quiet = True)
+	ui.queue(concurrency_count = concurrency_count).launch(show_api = False, quiet = True, server_name='0.0.0.0', server_port=8288)
