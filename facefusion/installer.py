6824b1a56d90664c73b39c05967a5bdd9123653a
henryruhs <info@henryruhs.com>
2024-05-29 23:35:59 +0200

    Add feature flag

--- facefusion/installer.py
+++ facefusion/installer.py
@@ -15,17 +15,17 @@ if is_macos():
 ONNXRUNTIMES : Dict[str, Tuple[str, str]] = {}
 
 if is_macos():
-	ONNXRUNTIMES['default'] = ('onnxruntime', '1.17.3')
+	ONNXRUNTIMES['default'] = ('onnxruntime', '1.18.0')
 else:
-	ONNXRUNTIMES['default'] = ('onnxruntime', '1.17.3')
-	ONNXRUNTIMES['cuda-12.2'] = ('onnxruntime-gpu', '1.17.1')
-	ONNXRUNTIMES['cuda-11.8'] = ('onnxruntime-gpu', '1.17.1')
+	ONNXRUNTIMES['default'] = ('onnxruntime', '1.18.0')
+	ONNXRUNTIMES['cuda-12.2'] = ('onnxruntime-gpu', '1.18.0')
+	ONNXRUNTIMES['cuda-11.8'] = ('onnxruntime-gpu', '1.18.0')
 	ONNXRUNTIMES['openvino'] = ('onnxruntime-openvino', '1.15.0')
 if is_linux():
 	ONNXRUNTIMES['rocm-5.4.2'] = ('onnxruntime-rocm', '1.16.3')
 	ONNXRUNTIMES['rocm-5.6'] = ('onnxruntime-rocm', '1.16.3')
 if is_windows():
-	ONNXRUNTIMES['directml'] = ('onnxruntime-directml', '1.17.3')
+	ONNXRUNTIMES['directml'] = ('onnxruntime-directml', '1.18.0')
 
 
 def cli() -> None:
