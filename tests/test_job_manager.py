2bce29a33000dfa58c28eeab81da498130d19945
henryruhs <info@henryruhs.com>
2024-06-03 15:12:56 +0200

    Change order

--- tests/test_job_manager.py
+++ tests/test_job_manager.py
@@ -47,6 +47,33 @@ def test_add_step() -> None:
 	assert get_total_steps('test_job_add_step') == 1
 
 
+def test_insert_step() -> None:
+	copy_json('tests/providers/test_job_insert_step.json', './.jobs/queued/test_job_insert_step.json')
+	assert get_total_steps('test_job_insert_step') == 1
+
+	step =\
+	{
+		'source_paths': [ '123.jpg' ],
+		'target_path': '456.jpg',
+		'output_path': './output'
+	}
+	assert insert_step('test_job_insert_step', 0, step)
+	assert get_total_steps('test_job_insert_step') == 2
+	job = read_json('./.jobs/queued/test_job_insert_step.json')
+	assert job.get('steps')[0].get('args') == step
+
+	step =\
+	{
+		'source_paths': [ 'abc.jpg' ],
+		'target_path': 'def.jpg',
+		'output_path': './output'
+	}
+	assert insert_step('test_job_insert_step', -1, step)
+	assert get_total_steps('test_job_insert_step') == 3
+	job = read_json('./.jobs/queued/test_job_insert_step.json')
+	assert job.get('steps')[-1].get('args') == step
+
+
 def test_remove_step() -> None:
 	copy_json('tests/providers/test_job_remove_step.json', './.jobs/queued/test_job_remove_step.json')
 	assert get_total_steps('test_job_remove_step') == 3
@@ -72,35 +99,9 @@ def test_remove_step() -> None:
 	}
 
 
-def test_insert_step() -> None:
-	copy_json('tests/providers/test_job_insert_step.json', './.jobs/queued/test_job_insert_step.json')
-	assert get_total_steps('test_job_insert_step') == 1
-
-	step = \
-	{
-		'source_paths': ['123.jpg'],
-		'target_path': '456.jpg',
-		'output_path': './output'
-	}
-	assert insert_step('test_job_insert_step', 0, step)
-	assert get_total_steps('test_job_insert_step') == 2
-	job = read_json('./.jobs/queued/test_job_insert_step.json')
-	assert job.get('steps')[0].get('args') == step
-
-	step =\
-	{
-		'source_paths': ['abc.jpg'],
-		'target_path': 'def.jpg',
-		'output_path': './output'
-	}
-	assert insert_step('test_job_insert_step', -1, step)
-	assert get_total_steps('test_job_insert_step') == 3
-	job = read_json('./.jobs/queued/test_job_insert_step.json')
-	assert job.get('steps')[-1].get('args') == step
-
-
 def test_move_job() -> None:
 	copy_json('tests/providers/test_move_job.json', './.jobs/queued/test_move_job.json')
+
 	assert move_job_file('test_move_job', 'failed')
 	assert get_job_status('test_move_job') == 'failed'
 	assert move_job_file('test_move_job', 'completed')
