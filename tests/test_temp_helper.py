1659f42e092d9df1f0cc419c65702ed8a8931fe6
henryruhs <info@henryruhs.com>
2024-06-06 09:51:44 +0200

    Fix temp helper testing

--- tests/test_temp_helper.py
+++ tests/test_temp_helper.py
@@ -5,7 +5,7 @@ import pytest
 
 import facefusion.globals
 from facefusion.download import conditional_download
-from facefusion.temp_helper import get_temp_file_path, get_temp_directory_path, get_temp_frame_paths, get_temp_frames_pattern
+from facefusion.temp_helper import get_temp_file_path, get_temp_directory_path, get_temp_frames_pattern
 
 
 @pytest.fixture(scope = 'module', autouse = True)
@@ -29,4 +29,4 @@ def test_get_temp_directory_path() -> None:
 
 def test_get_temp_frames_pattern() -> None:
 	temp_directory = tempfile.gettempdir()
-	assert get_temp_frames_pattern('.assets/examples/target-240p.mp4', '%04d') == os.path.join(temp_directory, '/tmp/facefusion/target-240p/%04d.png')
+	assert get_temp_frames_pattern('.assets/examples/target-240p.mp4', '%04d') == os.path.join(temp_directory, 'facefusion/target-240p/%04d.png')
