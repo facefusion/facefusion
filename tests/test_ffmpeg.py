3b87416600d3aa69755740ee1741a52f9c8b7500
henryruhs <info@henryruhs.com>
2024-06-07 12:02:54 +0200

    Rework testing suite files part3

--- tests/test_ffmpeg.py
+++ tests/test_ffmpeg.py
@@ -7,21 +7,22 @@ from facefusion import process_manager
 from facefusion.temp_helper import get_temp_directory_path, create_temp, clear_temp
 from facefusion.download import conditional_download
 from facefusion.ffmpeg import extract_frames, concat_video, read_audio_buffer
+from .helper import get_test_examples_directory, prepare_test_output_directory, get_test_example_file, get_test_output_file
 
 
 @pytest.fixture(scope = 'module', autouse = True)
 def before_all() -> None:
 	process_manager.start()
-	conditional_download('.assets/examples',
+	conditional_download(get_test_examples_directory(),
 	[
 		'https://github.com/facefusion/facefusion-assets/releases/download/examples/source.jpg',
 		'https://github.com/facefusion/facefusion-assets/releases/download/examples/source.mp3',
 		'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-240p.mp4'
 	])
-	subprocess.run([ 'ffmpeg', '-i', '.assets/examples/source.mp3', '.assets/examples/source.wav' ])
-	subprocess.run([ 'ffmpeg', '-i', '.assets/examples/target-240p.mp4', '-vf', 'fps=25', '.assets/examples/target-240p-25fps.mp4' ])
-	subprocess.run([ 'ffmpeg', '-i', '.assets/examples/target-240p.mp4', '-vf', 'fps=30', '.assets/examples/target-240p-30fps.mp4' ])
-	subprocess.run([ 'ffmpeg', '-i', '.assets/examples/target-240p.mp4', '-vf', 'fps=60', '.assets/examples/target-240p-60fps.mp4' ])
+	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('source.mp3'), get_test_example_file('source.wav') ])
+	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('target-240p.mp4'), '-vf', 'fps=25', get_test_example_file('target-240p-25fps.mp4') ])
+	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('target-240p.mp4'), '-vf', 'fps=30', get_test_example_file('target-240p-30fps.mp4') ])
+	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('target-240p.mp4'), '-vf', 'fps=60', get_test_example_file('target-240p-60fps.mp4') ])
 
 
 @pytest.fixture(scope = 'function', autouse = True)
@@ -29,14 +30,15 @@ def before_each() -> None:
 	facefusion.globals.trim_frame_start = None
 	facefusion.globals.trim_frame_end = None
 	facefusion.globals.temp_frame_format = 'jpg'
+	prepare_test_output_directory()
 
 
 def test_extract_frames() -> None:
 	target_paths =\
 	[
-		'.assets/examples/target-240p-25fps.mp4',
-		'.assets/examples/target-240p-30fps.mp4',
-		'.assets/examples/target-240p-60fps.mp4'
+		get_test_example_file('target-240p-25fps.mp4'),
+		get_test_example_file('target-240p-30fps.mp4'),
+		get_test_example_file('target-240p-60fps.mp4')
 	]
 
 	for target_path in target_paths:
@@ -53,9 +55,9 @@ def test_extract_frames_with_trim_start() -> None:
 	facefusion.globals.trim_frame_start = 224
 	providers =\
 	[
-		('.assets/examples/target-240p-25fps.mp4', 55),
-		('.assets/examples/target-240p-30fps.mp4', 100),
-		('.assets/examples/target-240p-60fps.mp4', 212)
+		(get_test_example_file('target-240p-25fps.mp4'), 55),
+		(get_test_example_file('target-240p-30fps.mp4'), 100),
+		(get_test_example_file('target-240p-60fps.mp4'), 212)
 	]
 
 	for target_path, frame_total in providers:
@@ -73,9 +75,9 @@ def test_extract_frames_with_trim_start_and_trim_end() -> None:
 	facefusion.globals.trim_frame_end = 224
 	providers =\
 	[
-		('.assets/examples/target-240p-25fps.mp4', 120),
-		('.assets/examples/target-240p-30fps.mp4', 100),
-		('.assets/examples/target-240p-60fps.mp4', 50)
+		(get_test_example_file('target-240p-25fps.mp4'), 120),
+		(get_test_example_file('target-240p-30fps.mp4'), 100),
+		(get_test_example_file('target-240p-60fps.mp4'), 50)
 	]
 
 	for target_path, frame_total in providers:
@@ -92,9 +94,9 @@ def test_extract_frames_with_trim_end() -> None:
 	facefusion.globals.trim_frame_end = 100
 	providers =\
 	[
-		('.assets/examples/target-240p-25fps.mp4', 120),
-		('.assets/examples/target-240p-30fps.mp4', 100),
-		('.assets/examples/target-240p-60fps.mp4', 50)
+		(get_test_example_file('target-240p-25fps.mp4'), 120),
+		(get_test_example_file('target-240p-30fps.mp4'), 100),
+		(get_test_example_file('target-240p-60fps.mp4'), 50)
 	]
 
 	for target_path, frame_total in providers:
@@ -110,15 +112,15 @@ def test_extract_frames_with_trim_end() -> None:
 def test_concat_video() -> None:
 	target_paths =\
 	[
-		'.assets/examples/target-240p.mp4',
-		'.assets/examples/target-240p.mp4'
+		get_test_example_file('target-240p.mp4'),
+		get_test_example_file('target-240p.mp4')
 	]
-	output_path = '.assets/examples/test-concat-video.mp4'
+	output_path = get_test_output_file('test-concat-video.mp4')
 
 	assert concat_video(target_paths, output_path) is True
 
 
 def test_read_audio_buffer() -> None:
-	assert isinstance(read_audio_buffer('.assets/examples/source.mp3', 1, 1), bytes)
-	assert isinstance(read_audio_buffer('.assets/examples/source.wav', 1, 1), bytes)
-	assert read_audio_buffer('.assets/examples/invalid.mp3', 1, 1) is None
+	assert isinstance(read_audio_buffer(get_test_example_file('source.mp3'), 1, 1), bytes)
+	assert isinstance(read_audio_buffer(get_test_example_file('source.wav'), 1, 1), bytes)
+	assert read_audio_buffer(get_test_example_file('invalid.mp3'), 1, 1) is None
