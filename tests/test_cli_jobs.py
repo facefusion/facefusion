80ece7873fc9e88f614a607f0a3e9d93f66a796d
henryruhs <info@henryruhs.com>
2024-06-11 17:28:55 +0200

    Most tested Python code ever part3

--- tests/test_cli_jobs.py
+++ tests/test_cli_jobs.py
@@ -4,7 +4,7 @@ import pytest
 
 from facefusion.download import conditional_download
 from facefusion.job_manager import clear_jobs, init_jobs, count_step_total
-from .helper import get_test_jobs_directory, get_test_examples_directory, prepare_test_output_directory, get_test_example_file, is_test_job_file
+from .helper import get_test_jobs_directory, get_test_examples_directory, prepare_test_output_directory, is_test_job_file, get_test_example_file, get_test_output_file, is_test_output_file
 
 
 @pytest.fixture(scope = 'module', autouse = True)
@@ -132,7 +132,6 @@ def test_job_remove_step() -> None:
 	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-remove-step', 'test-job-remove-step', '0' ]
 
 	assert subprocess.run(commands).returncode == 1
-	assert count_step_total('test-job-remove-step') == 0
 
 	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-create', 'test-job-remove-step' ]
 	subprocess.run(commands)
@@ -141,7 +140,7 @@ def test_job_remove_step() -> None:
 	subprocess.run(commands)
 	subprocess.run(commands)
 
-	commands = [sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-remove-step', 'test-job-remove-step', '0' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-remove-step', 'test-job-remove-step', '0' ]
 
 	assert count_step_total('test-job-remove-step') == 2
 	assert subprocess.run(commands).returncode == 0
@@ -154,9 +153,29 @@ def test_job_remove_step() -> None:
 	assert count_step_total('test-job-remove-step') == 0
 
 
-@pytest.mark.skip()
 def test_job_run() -> None:
-	pass
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-run', 'test-job-run' ]
+
+	assert subprocess.run(commands).returncode == 1
+
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-create', 'test-job-run' ]
+	subprocess.run(commands)
+
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-run', '--frame-processors', 'face_debugger', '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-run.jpg') ]
+	subprocess.run(commands)
+
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-run', 'test-job-run' ]
+
+	assert subprocess.run(commands).returncode == 1
+
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-submit', 'test-job-run' ]
+	subprocess.run(commands)
+
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-run', 'test-job-run' ]
+
+	assert subprocess.run(commands).returncode == 0
+	assert subprocess.run(commands).returncode == 1
+	assert is_test_output_file('test-job-run.jpg') is True
 
 
 @pytest.mark.skip()
