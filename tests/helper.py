488550fb0b726cb9a0396194b8bcc580bce5ea19
henryruhs <info@henryruhs.com>
2024-06-14 23:13:33 +0200

    Sorting out temp resources

--- tests/helper.py
+++ tests/helper.py
@@ -1,21 +1,21 @@
 import os
 import shutil
-import tempfile
 
+from facefusion.temp_helper import get_temp_base_path
 from facefusion.typing import JobStatus
 from facefusion.filesystem import is_file
 
 
 def get_test_jobs_directory() -> str:
-	return os.path.join(tempfile.gettempdir(), 'test-jobs')
+	return os.path.join(get_temp_base_path(), 'test-jobs')
 
 
 def get_test_examples_directory() -> str:
-	return os.path.join(tempfile.gettempdir(), 'test-examples')
+	return os.path.join(get_temp_base_path(), 'test-examples')
 
 
 def get_test_outputs_directory() -> str:
-	return os.path.join(tempfile.gettempdir(), 'test-outputs')
+	return os.path.join(get_temp_base_path(), 'test-outputs')
 
 
 def prepare_test_output_directory() -> None:
@@ -24,15 +24,15 @@ def prepare_test_output_directory() -> None:
 
 
 def get_test_job_file(file : str, job_status : JobStatus) -> str:
-	return os.path.join(tempfile.gettempdir(), 'test-jobs', job_status, file)
+	return os.path.join(get_temp_base_path(), 'test-jobs', job_status, file)
 
 
 def get_test_example_file(file : str) -> str:
-	return os.path.join(tempfile.gettempdir(), 'test-examples', file)
+	return os.path.join(get_temp_base_path(), 'test-examples', file)
 
 
 def get_test_output_file(file : str) -> str:
-	return os.path.join(tempfile.gettempdir(), 'test-outputs', file)
+	return os.path.join(get_temp_base_path(), 'test-outputs', file)
 
 
 def is_test_job_file(file : str, job_status : JobStatus) -> bool:
