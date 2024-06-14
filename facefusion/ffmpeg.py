30036eb09abbed3e096a34bfc1c835d869d0343c
Henry Ruhs <info@henryruhs.com>
2024-06-14 09:46:49 +0200

    Make run_ffmpeg return the subprocess rather than a boolean (#582)
    * Make run_ffmpeg return the subprocess rather than a boolean
    
    * Fix benchmark

--- facefusion/ffmpeg.py
+++ facefusion/ffmpeg.py
@@ -11,20 +11,20 @@ from facefusion.temp_helper import get_temp_frames_pattern, get_temp_file_path
 from facefusion.vision import restrict_video_fps
 
 
-def run_ffmpeg(args : List[str]) -> bool:
+def run_ffmpeg(args : List[str]) -> subprocess.Popen[bytes]:
 	commands = [ 'ffmpeg', '-hide_banner', '-loglevel', 'error' ]
 	commands.extend(args)
 	process = subprocess.Popen(commands, stderr = subprocess.PIPE, stdout = subprocess.PIPE)
 
 	while process_manager.is_processing():
+		if facefusion.globals.log_level == 'debug':
+			log_debug(process)
 		try:
-			if facefusion.globals.log_level == 'debug':
-				log_debug(process)
-			return process.wait(timeout = 0.5) == 0
+			process.wait(timeout = 0.5)
 		except subprocess.TimeoutExpired:
 			continue
-	process.communicate()
-	return process.returncode == 0
+		return process
+	return process
 
 
 def open_ffmpeg(args : List[str]) -> subprocess.Popen[bytes]:
@@ -48,16 +48,16 @@ def extract_frames(target_path : str, temp_video_resolution : str, temp_video_fp
 	temp_frames_pattern = get_temp_frames_pattern(target_path, '%04d')
 	commands = [ '-i', target_path, '-s', str(temp_video_resolution), '-q:v', '0' ]
 
-	if trim_frame_start is not None and trim_frame_end is not None:
+	if isinstance(trim_frame_start, int) and isinstance(trim_frame_end, int):
 		commands.extend([ '-vf', 'trim=start_frame=' + str(trim_frame_start) + ':end_frame=' + str(trim_frame_end) + ',fps=' + str(temp_video_fps) ])
-	elif trim_frame_start is not None:
+	elif isinstance(trim_frame_start, int):
 		commands.extend([ '-vf', 'trim=start_frame=' + str(trim_frame_start) + ',fps=' + str(temp_video_fps) ])
-	elif trim_frame_end is not None:
+	elif isinstance(trim_frame_end, int):
 		commands.extend([ '-vf', 'trim=end_frame=' + str(trim_frame_end) + ',fps=' + str(temp_video_fps) ])
 	else:
 		commands.extend([ '-vf', 'fps=' + str(temp_video_fps) ])
 	commands.extend([ '-vsync', '0', temp_frames_pattern ])
-	return run_ffmpeg(commands)
+	return run_ffmpeg(commands).returncode == 0
 
 
 def merge_video(target_path : str, output_video_resolution : str, output_video_fps : Fps) -> bool:
@@ -79,7 +79,7 @@ def merge_video(target_path : str, output_video_resolution : str, output_video_f
 		output_video_compression = round(51 - (facefusion.globals.output_video_quality * 0.51))
 		commands.extend([ '-qp_i', str(output_video_compression), '-qp_p', str(output_video_compression), '-quality', map_amf_preset(facefusion.globals.output_video_preset) ])
 	commands.extend([ '-vf', 'framerate=fps=' + str(output_video_fps), '-pix_fmt', 'yuv420p', '-colorspace', 'bt709', '-y', temp_file_path ])
-	return run_ffmpeg(commands)
+	return run_ffmpeg(commands).returncode == 0
 
 
 def concat_video(output_path : str, temp_output_paths : List[str]) -> bool:
@@ -87,21 +87,23 @@ def concat_video(output_path : str, temp_output_paths : List[str]) -> bool:
 		for temp_output_path in temp_output_paths:
 			concat_video_file.write('file \'' + os.path.abspath(temp_output_path) + '\'' + os.linesep)
 	commands = [ '-f', 'concat', '-safe', '0', '-i', concat_video_file.name, '-c', 'copy', '-y', os.path.abspath(output_path) ]
-	return run_ffmpeg(commands)
+	process = run_ffmpeg(commands)
+	process.communicate()
+	return process.returncode == 0
 
 
 def copy_image(target_path : str, temp_image_resolution : str) -> bool:
 	temp_file_path = get_temp_file_path(target_path)
 	temp_image_compression = calc_image_compression(target_path, 100)
 	commands = [ '-i', target_path, '-s', str(temp_image_resolution), '-q:v', str(temp_image_compression), '-y', temp_file_path ]
-	return run_ffmpeg(commands)
+	return run_ffmpeg(commands).returncode == 0
 
 
 def finalize_image(target_path : str, output_path : str, output_image_resolution : str) -> bool:
 	temp_file_path = get_temp_file_path(target_path)
 	output_image_compression = calc_image_compression(target_path, facefusion.globals.output_image_quality)
 	commands = [ '-i', temp_file_path, '-s', str(output_image_resolution), '-q:v', str(output_image_compression), '-y', output_path ]
-	return run_ffmpeg(commands)
+	return run_ffmpeg(commands).returncode == 0
 
 
 def calc_image_compression(image_path : str, image_quality : int) -> int:
@@ -126,20 +128,20 @@ def restore_audio(target_path : str, output_path : str, output_video_fps : Fps) 
 	temp_file_path = get_temp_file_path(target_path)
 	commands = [ '-i', temp_file_path ]
 
-	if trim_frame_start is not None:
+	if isinstance(trim_frame_start, int):
 		start_time = trim_frame_start / output_video_fps
 		commands.extend([ '-ss', str(start_time) ])
-	if trim_frame_end is not None:
+	if isinstance(trim_frame_end, int):
 		end_time = trim_frame_end / output_video_fps
 		commands.extend([ '-to', str(end_time) ])
 	commands.extend([ '-i', target_path, '-c', 'copy', '-map', '0:v:0', '-map', '1:a:0', '-shortest', '-y', output_path ])
-	return run_ffmpeg(commands)
+	return run_ffmpeg(commands).returncode == 0
 
 
 def replace_audio(target_path : str, audio_path : str, output_path : str) -> bool:
 	temp_file_path = get_temp_file_path(target_path)
 	commands = [ '-i', temp_file_path, '-i', audio_path, '-af', 'apad', '-shortest', '-y', output_path ]
-	return run_ffmpeg(commands)
+	return run_ffmpeg(commands).returncode == 0
 
 
 def map_nvenc_preset(output_video_preset : OutputVideoPreset) -> Optional[str]:
