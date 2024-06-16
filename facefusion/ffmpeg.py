53cb13ef191e5d08bfa56fdce47ac900a9bd6a96
henryruhs <info@henryruhs.com>
2024-06-16 21:22:44 +0200

    Windows does not like tempfile.NamedTemporaryFile

--- facefusion/ffmpeg.py
+++ facefusion/ffmpeg.py
@@ -7,7 +7,7 @@ import tempfile
 import facefusion.globals
 from facefusion import logger, process_manager
 from facefusion.typing import OutputVideoPreset, Fps, AudioBuffer
-from facefusion.temp_helper import get_temp_file_path, get_temp_frames_pattern
+from facefusion.temp_helper import get_temp_base_path, get_temp_file_path, get_temp_frames_pattern
 from facefusion.vision import restrict_video_fps
 
 
@@ -83,14 +83,18 @@ def merge_video(target_path : str, output_video_resolution : str, output_video_f
 
 
 def concat_video(output_path : str, temp_output_paths : List[str]) -> bool:
-	with tempfile.NamedTemporaryFile(mode = 'w') as concat_video_file:
+	concat_video_path = tempfile.mktemp(dir = get_temp_base_path())
+
+	with open(concat_video_path, 'w') as concat_video_file:
 		for temp_output_path in temp_output_paths:
 			concat_video_file.write('file \'' + temp_output_path + '\'' + os.linesep)
 		concat_video_file.flush()
-		commands = [ '-f', 'concat', '-safe', '0', '-i', concat_video_file.name, '-c:v', 'copy', '-c:a', 'aac', '-y', output_path ]
-		process = run_ffmpeg(commands)
-		process.communicate()
-		return process.returncode == 0
+		concat_video_file.close()
+	commands = [ '-f', 'concat', '-safe', '0', '-i', concat_video_file.name, '-c:v', 'copy', '-c:a', 'aac', '-y', output_path ]
+	process = run_ffmpeg(commands)
+	process.communicate()
+	os.remove(concat_video_path)
+	return process.returncode == 0
 
 
 def copy_image(target_path : str, temp_image_resolution : str) -> bool:
