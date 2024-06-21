f3fd97b0d04cf797f6e24a1b47916675f5ef2d66
Aleksei Shevchenko <i.am.skeeet@gmail.com>
2024-06-21 06:01:16 -0700

    Add 'hvc1' tag to x265/hevc videos

--- facefusion/ffmpeg.py
+++ facefusion/ffmpeg.py
@@ -63,6 +63,9 @@ def merge_video(target_path : str, output_video_resolution : str, output_video_f
 	temp_file_path = get_temp_file_path(target_path)
 	temp_frames_pattern = get_temp_frames_pattern(target_path, '%04d')
 	commands = [ '-r', str(temp_video_fps), '-i', temp_frames_pattern, '-s', str(output_video_resolution), '-c:v', facefusion.globals.output_video_encoder ]
+	
+	if facefusion.globals.output_video_encoder in [ 'hevc_nvenc', 'libx265', 'hevc_amf']:
+			commands.extend(['-tag:v', 'hvc1'])
 
 	if facefusion.globals.output_video_encoder in [ 'libx264', 'libx265' ]:
 		output_video_compression = round(51 - (facefusion.globals.output_video_quality * 0.51))
