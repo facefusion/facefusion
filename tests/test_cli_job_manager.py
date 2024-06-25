b78ef302263a51c5672f39a9a74badb9c09f77eb
henryruhs <info@henryruhs.com>
2024-06-25 23:30:58 +0200

    Fix testing for cli jobs manager

--- tests/test_cli_job_manager.py
+++ tests/test_cli_job_manager.py
@@ -2,8 +2,19 @@ import subprocess
 import sys
 import pytest
 
+from facefusion.download import conditional_download
 from facefusion.jobs.job_manager import clear_jobs, init_jobs, count_step_total
-from .helper import get_test_jobs_directory, is_test_job_file
+from .helper import get_test_jobs_directory, get_test_examples_directory, get_test_example_file, get_test_output_file, is_test_job_file
+
+
+@pytest.fixture(scope = 'module', autouse = True)
+def before_all() -> None:
+	conditional_download(get_test_examples_directory(),
+	[
+		'https://github.com/facefusion/facefusion-assets/releases/download/examples/source.jpg',
+		'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-240p.mp4'
+	])
+	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('target-240p.mp4'), '-vframes', '1', get_test_example_file('target-240p.jpg') ])
 
 
 @pytest.fixture(scope = 'function', autouse = True)
@@ -33,7 +44,7 @@ def test_job_submit() -> None:
 
 	assert subprocess.run(commands).returncode == 1
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-submit' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-submit', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 	subprocess.run(commands)
 
 	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-submit', 'test-job-submit' ]
@@ -56,10 +67,10 @@ def test_submit_all() -> None:
 
 	assert subprocess.run(commands).returncode == 1
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-submit-all-1' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-submit-all-1', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 	subprocess.run(commands)
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-submit-all-2' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-submit-all-2', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 	subprocess.run(commands)
 
 	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-submit-all' ]
@@ -105,7 +116,7 @@ def test_job_delete_all() -> None:
 
 
 def test_job_add_step() -> None:
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-add-step' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-add-step', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 
 	assert subprocess.run(commands).returncode == 1
 	assert count_step_total('test-job-add-step') == 0
@@ -113,14 +124,14 @@ def test_job_add_step() -> None:
 	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-create', 'test-job-add-step' ]
 	subprocess.run(commands)
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-add-step' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-add-step', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 
 	assert subprocess.run(commands).returncode == 0
 	assert count_step_total('test-job-add-step') == 1
 
 
 def test_job_remix() -> None:
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-remix-step', 'test-job-remix-step', '0' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-remix-step', 'test-job-remix-step', '0', '-s', get_test_example_file('source.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 
 	assert subprocess.run(commands).returncode == 1
 	assert count_step_total('test-job-remix-step') == 0
@@ -128,23 +139,23 @@ def test_job_remix() -> None:
 	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-create', 'test-job-remix-step' ]
 	subprocess.run(commands)
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-remix-step' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-remix-step', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 	subprocess.run(commands)
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-remix-step', 'test-job-remix-step', '0' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-remix-step', 'test-job-remix-step', '0', '-s', get_test_example_file('source.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 
 	assert count_step_total('test-job-remix-step') == 1
 	assert subprocess.run(commands).returncode == 0
 	assert count_step_total('test-job-remix-step') == 2
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-remix-step', 'test-job-remix-step', '-1' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-remix-step', 'test-job-remix-step', '-1', '-s', get_test_example_file('source.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 
 	assert subprocess.run(commands).returncode == 0
 	assert count_step_total('test-job-remix-step') == 3
 
 
 def test_job_insert_step() -> None:
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-insert-step', 'test-job-insert-step', '0' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-insert-step', 'test-job-insert-step', '0', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 
 	assert subprocess.run(commands).returncode == 1
 	assert count_step_total('test-job-insert-step') == 0
@@ -152,16 +163,16 @@ def test_job_insert_step() -> None:
 	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-create', 'test-job-insert-step' ]
 	subprocess.run(commands)
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-insert-step' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-insert-step', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 	subprocess.run(commands)
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-insert-step', 'test-job-insert-step', '0' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-insert-step', 'test-job-insert-step', '0', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 
 	assert count_step_total('test-job-insert-step') == 1
 	assert subprocess.run(commands).returncode == 0
 	assert count_step_total('test-job-insert-step') == 2
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-insert-step', 'test-job-insert-step', '-1' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-insert-step', 'test-job-insert-step', '-1', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 
 	assert subprocess.run(commands).returncode == 0
 	assert count_step_total('test-job-insert-step') == 3
@@ -175,7 +186,7 @@ def test_job_remove_step() -> None:
 	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-create', 'test-job-remove-step' ]
 	subprocess.run(commands)
 
-	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-remove-step' ]
+	commands = [ sys.executable, 'run.py', '-j', get_test_jobs_directory(), '--job-add-step', 'test-job-remove-step', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.jpg'), '-o', get_test_output_file('test-job-remix-step.jpg') ]
 	subprocess.run(commands)
 	subprocess.run(commands)
 
