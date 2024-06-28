1e374bbd895cee1b16ca9b1ea6400cf9d5a1cefb
henryruhs <info@henryruhs.com>
2024-06-28 18:23:06 +0200

    Dynamic output file

--- facefusion/uis/components/output.py
+++ facefusion/uis/components/output.py
@@ -13,7 +13,7 @@ from facefusion.jobs import job_manager, job_runner, job_store, job_helper
 from facefusion.program_helper import reduce_args, import_globals
 from facefusion.filesystem import is_image, is_video, is_directory
 from facefusion.temp_helper import clear_temp_directory
-import facefusion.processors.frame
+from facefusion.processors.frame import globals as frame_processors_globals
 
 OUTPUT_PATH_TEXTBOX : Optional[gradio.Textbox] = None
 OUTPUT_IMAGE : Optional[gradio.Image] = None
@@ -76,33 +76,38 @@ def start() -> Tuple[gradio.Button, gradio.Button]:
 
 
 def process() -> Tuple[gradio.Image, gradio.Video, gradio.Button, gradio.Button]:
+	output_path = facefusion.globals.output_path
+	stored_output_path = facefusion.globals.output_path
+
 	if facefusion.globals.system_memory_limit > 0:
 		limit_system_memory(facefusion.globals.system_memory_limit)
-	if is_directory(facefusion.globals.output_path):
-		facefusion.globals.output_path = suggest_output_path(facefusion.globals.output_path, facefusion.globals.target_path)
+	if is_directory(output_path):
+		output_path = suggest_output_path(output_path, facefusion.globals.target_path)
 	if job_manager.init_jobs(facefusion.globals.jobs_path):
-		create_and_run_job()
-	if is_image(facefusion.globals.output_path):
-		return gradio.Image(value = facefusion.globals.output_path, visible = True), gradio.Video(value = None, visible = False), gradio.Button(visible = True), gradio.Button(visible = False)
-	if is_video(facefusion.globals.output_path):
-		return gradio.Image(value = None, visible = False), gradio.Video(value = facefusion.globals.output_path, visible = True), gradio.Button(visible = True), gradio.Button(visible = False)
+		create_and_run_job(output_path)
+		facefusion.globals.output_path = stored_output_path
+	if is_image(output_path):
+		return gradio.Image(value = output_path, visible = True), gradio.Video(value = None, visible = False), gradio.Button(visible = True), gradio.Button(visible = False)
+	if is_video(output_path):
+		return gradio.Image(value = None, visible = False), gradio.Video(value = output_path, visible = True), gradio.Button(visible = True), gradio.Button(visible = False)
 	return gradio.Image(value = None), gradio.Video(value = None), gradio.Button(visible = True), gradio.Button(visible = False)
 
 
 def suggest_output_path(output_directory_path : str, target_path : str) -> Optional[str]:
 	if is_image(target_path) or is_video(target_path):
 		_, target_extension = os.path.splitext(target_path)
-		output_name = hashlib.sha1(str(facefusion.globals.__dict__).encode('utf-8')).hexdigest()[:8]
+		output_name = hashlib.sha1(str([ facefusion.globals.__dict__, frame_processors_globals.__dict__ ]).encode('utf-8')).hexdigest()[:8]
 		return os.path.join(output_directory_path, output_name + target_extension)
 	return None
 
 
-def create_and_run_job() -> bool:
+def create_and_run_job(output_path : str) -> bool:
 	job_id = job_helper.suggest_job_id('ui')
 	program = create_program()
 	program = import_globals(program, job_store.get_step_keys(), [ facefusion, facefusion.processors.frame ])
 	program = reduce_args(program, job_store.get_step_keys())
 	step_args = vars(program.parse_args())
+	step_args['output_path'] = output_path
 
 	return job_manager.create_job(job_id) and job_manager.add_step(job_id, step_args) and job_manager.submit_job(job_id) and job_runner.run_job(job_id, process_step)
 
