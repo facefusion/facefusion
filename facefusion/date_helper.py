664680622e42c1e190c862c1b57cce639343c3af
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2024-07-03 19:22:57 +0530

    changes

--- facefusion/date_helper.py
+++ facefusion/date_helper.py
@@ -7,8 +7,8 @@ def get_current_datetime() -> str:
 	return datetime.now().astimezone().isoformat()
 
 
-def get_datetime_components(delta: timedelta) -> Tuple[int, int, int, int]:
-	total_seconds = int(delta.total_seconds())
+def get_datetime_components(date_time: timedelta) -> Tuple[int, int, int, int]:
+	total_seconds = int(date_time.total_seconds())
 	days, hours = divmod(total_seconds, 86400)
 	hours, minutes = divmod(hours, 3600)
 	minutes, seconds = divmod(minutes, 60)
@@ -16,15 +16,15 @@ def get_datetime_components(delta: timedelta) -> Tuple[int, int, int, int]:
 
 
 def describe_time_ago(date_time: str) -> Optional[str]:
-	datetime_parsed = datetime.fromisoformat(date_time)
-	datetime_current = datetime.now(datetime_parsed.tzinfo)
-	datetime_difference = datetime_current - datetime_parsed
-	days, hours, minutes, _ = get_datetime_components(datetime_difference)
+	date_time_parsed = datetime.fromisoformat(date_time)
+	date_time_current = datetime.now(date_time_parsed.tzinfo)
+	date_time_difference = date_time_current - date_time_parsed
+	days, hours, minutes, _ = get_datetime_components(date_time_difference)
 
-	if timedelta(days = 1) < datetime_difference:
+	if timedelta(days = 1) < date_time_difference:
 		return wording.get('time_ago_days').format(days = days, hours = hours, minutes = minutes)
-	if timedelta(hours = 1) < datetime_difference:
+	if timedelta(hours = 1) < date_time_difference:
 		return wording.get('time_ago_hours').format(hours = hours, minutes = minutes)
-	if timedelta(minutes = 1) < datetime_difference:
+	if timedelta(minutes = 1) < date_time_difference:
 		return wording.get('time_ago_minutes').format(minutes = minutes)
 	return wording.get('time_ago_now')
