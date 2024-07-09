7db02a9f12bf2530cc45b57228b8fe85d35a4a63
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2024-07-09 18:30:13 +0530

    changes

---
+++ facefusion/uis/layouts/jobs.py
@@ -0,0 +1,35 @@
+import gradio
+
+from facefusion import state_manager
+from facefusion.uis.components import about, job_list, job_runner
+
+
+def pre_check() -> bool:
+	return True
+
+
+def pre_render() -> bool:
+	return True
+
+
+def render() -> gradio.Blocks:
+	with gradio.Blocks() as layout:
+		with gradio.Row():
+			with gradio.Column(scale = 1):
+				with gradio.Blocks():
+					about.render()
+				with gradio.Blocks():
+					job_runner.render()
+			with gradio.Column(scale = 4):
+				with gradio.Blocks():
+					job_list.render()
+	return layout
+
+
+def listen() -> None:
+	job_runner.listen()
+	job_list.listen()
+
+
+def run(ui : gradio.Blocks) -> None:
+	ui.launch(show_api = False, inbrowser = state_manager.get_item('open_browser'))
