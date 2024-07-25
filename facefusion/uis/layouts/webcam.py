6458d3bf7ffddb1807b12aceca756930ce8bc94d
henryruhs <info@henryruhs.com>
2024-07-25 19:49:25 +0200

    Fix imports

--- facefusion/uis/layouts/webcam.py
+++ facefusion/uis/layouts/webcam.py
@@ -1,7 +1,7 @@
 import gradio
 
 from facefusion import state_manager
-from facefusion.uis.components import about, age_modifier_options, execution, execution_thread_count, face_debugger_options, face_enhancer_options, face_swapper_options, frame_colorizer_options, frame_enhancer_options, processors, lip_syncer_options, source, webcam, webcam_options
+from facefusion.uis.components import about, age_modifier_options, execution, execution_thread_count, face_debugger_options, face_enhancer_options, face_swapper_options, frame_colorizer_options, frame_enhancer_options, lip_syncer_options, processors, source, webcam, webcam_options
 
 
 def pre_check() -> bool:
