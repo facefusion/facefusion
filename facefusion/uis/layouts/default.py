75a8282e39f9df5418e0036a0cec2a9f078ecbca
henryruhs <info@henryruhs.com>
2024-08-07 17:15:04 +0200

    Introduce terminal to the UI part2

--- facefusion/uis/layouts/default.py
+++ facefusion/uis/layouts/default.py
@@ -1,7 +1,7 @@
 import gradio
 
 from facefusion import state_manager
-from facefusion.uis.components import about, age_modifier_options, common_options, execution, execution_queue_count, execution_thread_count, expression_restorer_options, face_analyser, face_debugger_options, face_editor_options, face_enhancer_options, face_masker, face_selector, face_swapper_options, frame_colorizer_options, frame_enhancer_options, instant_runner, job_manager, job_runner, terminal, lip_syncer_options, memory, output, output_options, preview, processors, source, target, temp_frame, trim_frame, ui_workflow
+from facefusion.uis.components import about, age_modifier_options, common_options, execution, execution_queue_count, execution_thread_count, expression_restorer_options, face_analyser, face_debugger_options, face_editor_options, face_enhancer_options, face_masker, face_selector, face_swapper_options, frame_colorizer_options, frame_enhancer_options, instant_runner, job_manager, job_runner, lip_syncer_options, memory, output, output_options, preview, processors, source, target, temp_frame, terminal, trim_frame, ui_workflow
 
 
 def pre_check() -> bool:
