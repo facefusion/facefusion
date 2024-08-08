f150f578721d04fb630c389edfe9830bcc6955da
henryruhs <info@henryruhs.com>
2024-08-09 01:00:27 +0200

    Finalize terminal UI

--- facefusion/uis/components/terminal.py
+++ facefusion/uis/components/terminal.py
@@ -1,14 +1,18 @@
 import io
 import logging
+import math
+import os
 from typing import Optional
 
 import gradio as gr
+from tqdm import tqdm
 
 from facefusion import logger, wording
 
 TERMINAL_TEXTBOX: Optional[gr.Textbox] = None
 LOG_BUFFER = io.StringIO()
 LOG_HANDLER = logging.StreamHandler(LOG_BUFFER)
+TQDM_UPDATE = tqdm.update
 
 
 def render() -> None:
@@ -17,8 +21,8 @@ def render() -> None:
 	TERMINAL_TEXTBOX = gr.Textbox(
 		label = wording.get('uis.terminal_textbox'),
 		value = read_logs,
-		lines = 4,
-		max_lines = 8,
+		lines = 10,
+		max_lines = 10,
 		every = 0.5,
 		show_copy_button = True
 	)
@@ -26,6 +30,24 @@ def render() -> None:
 
 def listen() -> None:
 	logger.get_package_logger().addHandler(LOG_HANDLER)
+	tqdm.update = tqdm_update
+
+
+def tqdm_update(self : tqdm, n : int = 1) -> None:
+	TQDM_UPDATE(self, n)
+	percentage = math.floor(self.n / self.total * 100)
+	output = self.desc + wording.get('colon') + ' ' + str(percentage) + '% (' + str(self.n) + '/' + str(self.total) + ')'
+
+	LOG_BUFFER.seek(0)
+	log_buffer = LOG_BUFFER.read()
+	lines = log_buffer.splitlines()
+	if lines and lines[-1].startswith(self.desc):
+		position = log_buffer.rfind(lines[-1])
+		LOG_BUFFER.seek(position)
+	else:
+		LOG_BUFFER.seek(0, os.SEEK_END)
+	LOG_BUFFER.write(output + os.linesep)
+	LOG_BUFFER.flush()
 
 
 def read_logs() -> str:
