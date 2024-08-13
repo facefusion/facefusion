ccf7b9b7f4df7c270e15d4d0545bf74f705777eb
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2024-08-13 23:42:37 +0530

    changes

--- facefusion/uis/components/face_editor_options.py
+++ facefusion/uis/components/face_editor_options.py
@@ -15,6 +15,12 @@ FACE_EDITOR_EYE_GAZE_HORIZONTAL_SLIDER : Optional[gradio.Slider] = None
 FACE_EDITOR_EYE_GAZE_VERTICAL_SLIDER : Optional[gradio.Slider] = None
 FACE_EDITOR_EYE_OPEN_RATIO_SLIDER : Optional[gradio.Slider] = None
 FACE_EDITOR_LIP_OPEN_RATIO_SLIDER : Optional[gradio.Slider] = None
+FACE_EDITOR_LIP_GRIM_SLIDER : Optional[gradio.Slider] = None
+FACE_EDITOR_LIP_POUT_SLIDER : Optional[gradio.Slider] = None
+FACE_EDITOR_LIP_PURSE_SLIDER : Optional[gradio.Slider] = None
+FACE_EDITOR_LIP_SMILE_SLIDER : Optional[gradio.Slider] = None
+FACE_EDITOR_LIP_HORIZONTAL_SLIDER : Optional[gradio.Slider] = None
+FACE_EDITOR_LIP_VERTICAL_SLIDER : Optional[gradio.Slider] = None
 
 
 def render() -> None:
@@ -24,6 +30,12 @@ def render() -> None:
 	global FACE_EDITOR_EYE_GAZE_VERTICAL_SLIDER
 	global FACE_EDITOR_EYE_OPEN_RATIO_SLIDER
 	global FACE_EDITOR_LIP_OPEN_RATIO_SLIDER
+	global FACE_EDITOR_LIP_GRIM_SLIDER
+	global FACE_EDITOR_LIP_POUT_SLIDER
+	global FACE_EDITOR_LIP_PURSE_SLIDER
+	global FACE_EDITOR_LIP_SMILE_SLIDER
+	global FACE_EDITOR_LIP_HORIZONTAL_SLIDER
+	global FACE_EDITOR_LIP_VERTICAL_SLIDER
 
 	FACE_EDITOR_MODEL_DROPDOWN = gradio.Dropdown(
 		label = wording.get('uis.face_editor_model_dropdown'),
@@ -71,12 +83,66 @@ def render() -> None:
 		maximum = processors_choices.face_editor_lip_open_ratio_range[-1],
 		visible = 'face_editor' in state_manager.get_item('processors'),
 	)
+	FACE_EDITOR_LIP_GRIM_SLIDER = gradio.Slider(
+		label = wording.get('uis.face_editor_lip_grim_slider'),
+		value = state_manager.get_item('face_editor_lip_grim'),
+		step = calc_float_step(processors_choices.face_editor_lip_grim_range),
+		minimum = processors_choices.face_editor_lip_grim_range[0],
+		maximum = processors_choices.face_editor_lip_grim_range[-1],
+		visible = 'face_editor' in state_manager.get_item('processors'),
+	)
+	FACE_EDITOR_LIP_POUT_SLIDER = gradio.Slider(
+		label = wording.get('uis.face_editor_lip_pout_slider'),
+		value = state_manager.get_item('face_editor_lip_pout'),
+		step = calc_float_step(processors_choices.face_editor_lip_pout_range),
+		minimum = processors_choices.face_editor_lip_pout_range[0],
+		maximum = processors_choices.face_editor_lip_pout_range[-1],
+		visible = 'face_editor' in state_manager.get_item('processors'),
+	)
+	FACE_EDITOR_LIP_PURSE_SLIDER = gradio.Slider(
+		label = wording.get('uis.face_editor_lip_purse_slider'),
+		value = state_manager.get_item('face_editor_lip_purse'),
+		step = calc_float_step(processors_choices.face_editor_lip_purse_range),
+		minimum = processors_choices.face_editor_lip_purse_range[0],
+		maximum = processors_choices.face_editor_lip_purse_range[-1],
+		visible = 'face_editor' in state_manager.get_item('processors'),
+	)
+	FACE_EDITOR_LIP_SMILE_SLIDER = gradio.Slider(
+		label = wording.get('uis.face_editor_lip_smile_slider'),
+		value = state_manager.get_item('face_editor_lip_smile'),
+		step = calc_float_step(processors_choices.face_editor_lip_smile_range),
+		minimum = processors_choices.face_editor_lip_smile_range[0],
+		maximum = processors_choices.face_editor_lip_smile_range[-1],
+		visible = 'face_editor' in state_manager.get_item('processors'),
+	)
+	FACE_EDITOR_LIP_HORIZONTAL_SLIDER = gradio.Slider(
+		label = wording.get('uis.face_editor_lip_horizontal_slider'),
+		value = state_manager.get_item('face_editor_lip_horizontal'),
+		step = calc_float_step(processors_choices.face_editor_lip_horizontal_range),
+		minimum = processors_choices.face_editor_lip_horizontal_range[0],
+		maximum = processors_choices.face_editor_lip_horizontal_range[-1],
+		visible = 'face_editor' in state_manager.get_item('processors'),
+	)
+	FACE_EDITOR_LIP_VERTICAL_SLIDER = gradio.Slider(
+		label = wording.get('uis.face_editor_lip_vertical_slider'),
+		value = state_manager.get_item('face_editor_lip_vertical'),
+		step = calc_float_step(processors_choices.face_editor_lip_vertical_range),
+		minimum = processors_choices.face_editor_lip_vertical_range[0],
+		maximum = processors_choices.face_editor_lip_vertical_range[-1],
+		visible = 'face_editor' in state_manager.get_item('processors'),
+	)
 	register_ui_component('face_editor_model_dropdown', FACE_EDITOR_MODEL_DROPDOWN)
 	register_ui_component('face_editor_eyebrow_direction_slider', FACE_EDITOR_EYEBROW_DIRECTION_SLIDER)
 	register_ui_component('face_editor_eye_gaze_horizontal_slider', FACE_EDITOR_EYE_GAZE_HORIZONTAL_SLIDER)
 	register_ui_component('face_editor_eye_gaze_vertical_slider', FACE_EDITOR_EYE_GAZE_VERTICAL_SLIDER)
 	register_ui_component('face_editor_eye_open_ratio_slider', FACE_EDITOR_EYE_OPEN_RATIO_SLIDER)
 	register_ui_component('face_editor_lip_open_ratio_slider', FACE_EDITOR_LIP_OPEN_RATIO_SLIDER)
+	register_ui_component('face_editor_lip_grim_slider', FACE_EDITOR_LIP_GRIM_SLIDER)
+	register_ui_component('face_editor_lip_pout_slider', FACE_EDITOR_LIP_POUT_SLIDER)
+	register_ui_component('face_editor_lip_purse_slider', FACE_EDITOR_LIP_PURSE_SLIDER)
+	register_ui_component('face_editor_lip_smile_slider', FACE_EDITOR_LIP_SMILE_SLIDER)
+	register_ui_component('face_editor_lip_horizontal_slider', FACE_EDITOR_LIP_HORIZONTAL_SLIDER)
+	register_ui_component('face_editor_lip_vertical_slider', FACE_EDITOR_LIP_VERTICAL_SLIDER)
 
 
 def listen() -> None:
@@ -86,15 +152,21 @@ def listen() -> None:
 	FACE_EDITOR_EYE_GAZE_VERTICAL_SLIDER.release(update_face_editor_eye_gaze_vertical, inputs = FACE_EDITOR_EYE_GAZE_VERTICAL_SLIDER)
 	FACE_EDITOR_EYE_OPEN_RATIO_SLIDER.release(update_face_editor_eye_open_ratio, inputs = FACE_EDITOR_EYE_OPEN_RATIO_SLIDER)
 	FACE_EDITOR_LIP_OPEN_RATIO_SLIDER.release(update_face_editor_lip_open_ratio, inputs = FACE_EDITOR_LIP_OPEN_RATIO_SLIDER)
+	FACE_EDITOR_LIP_GRIM_SLIDER.release(update_face_editor_lip_grim, inputs = FACE_EDITOR_LIP_GRIM_SLIDER)
+	FACE_EDITOR_LIP_POUT_SLIDER.release(update_face_editor_lip_pout, inputs = FACE_EDITOR_LIP_POUT_SLIDER)
+	FACE_EDITOR_LIP_PURSE_SLIDER.release(update_face_editor_lip_purse, inputs = FACE_EDITOR_LIP_PURSE_SLIDER)
+	FACE_EDITOR_LIP_SMILE_SLIDER.release(update_face_editor_lip_smile, inputs = FACE_EDITOR_LIP_SMILE_SLIDER)
+	FACE_EDITOR_LIP_HORIZONTAL_SLIDER.release(update_face_editor_lip_horizontal, inputs = FACE_EDITOR_LIP_HORIZONTAL_SLIDER)
+	FACE_EDITOR_LIP_VERTICAL_SLIDER.release(update_face_editor_lip_vertical, inputs = FACE_EDITOR_LIP_VERTICAL_SLIDER)
 
 	processors_checkbox_group = get_ui_component('processors_checkbox_group')
 	if processors_checkbox_group:
-		processors_checkbox_group.change(remote_update, inputs = processors_checkbox_group, outputs = [ FACE_EDITOR_MODEL_DROPDOWN, FACE_EDITOR_EYEBROW_DIRECTION_SLIDER, FACE_EDITOR_EYE_GAZE_HORIZONTAL_SLIDER, FACE_EDITOR_EYE_GAZE_VERTICAL_SLIDER, FACE_EDITOR_EYE_OPEN_RATIO_SLIDER, FACE_EDITOR_LIP_OPEN_RATIO_SLIDER ])
+		processors_checkbox_group.change(remote_update, inputs = processors_checkbox_group, outputs = [ FACE_EDITOR_MODEL_DROPDOWN, FACE_EDITOR_EYEBROW_DIRECTION_SLIDER, FACE_EDITOR_EYE_GAZE_HORIZONTAL_SLIDER, FACE_EDITOR_EYE_GAZE_VERTICAL_SLIDER, FACE_EDITOR_EYE_OPEN_RATIO_SLIDER, FACE_EDITOR_LIP_OPEN_RATIO_SLIDER, FACE_EDITOR_LIP_GRIM_SLIDER, FACE_EDITOR_LIP_POUT_SLIDER, FACE_EDITOR_LIP_PURSE_SLIDER, FACE_EDITOR_LIP_SMILE_SLIDER, FACE_EDITOR_LIP_HORIZONTAL_SLIDER, FACE_EDITOR_LIP_VERTICAL_SLIDER ])
 
 
-def remote_update(processors : List[str]) -> Tuple[gradio.Dropdown, gradio.Slider, gradio.Slider, gradio.Slider, gradio.Slider, gradio.Slider]:
+def remote_update(processors : List[str]) -> Tuple[gradio.Dropdown, gradio.Slider, gradio.Slider, gradio.Slider, gradio.Slider, gradio.Slider, gradio.Slider, gradio.Slider, gradio.Slider, gradio.Slider, gradio.Slider, gradio.Slider]:
 	has_face_editor = 'face_editor' in processors
-	return gradio.Dropdown(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor)
+	return gradio.Dropdown(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor), gradio.Slider(visible = has_face_editor)
 
 
 def update_face_editor_model(face_editor_model : FaceEditorModel) -> gradio.Dropdown:
@@ -125,3 +197,27 @@ def update_face_editor_eye_open_ratio(face_editor_eye_open_ratio : float) -> Non
 
 def update_face_editor_lip_open_ratio(face_editor_lip_open_ratio : float) -> None:
 	state_manager.set_item('face_editor_lip_open_ratio', face_editor_lip_open_ratio)
+
+
+def update_face_editor_lip_grim(face_editor_lip_grim : float) -> None:
+	state_manager.set_item('face_editor_lip_grim', face_editor_lip_grim)
+
+
+def update_face_editor_lip_pout(face_editor_lip_pout : float) -> None:
+	state_manager.set_item('face_editor_lip_pout', face_editor_lip_pout)
+
+
+def update_face_editor_lip_purse(face_editor_lip_purse : float) -> None:
+	state_manager.set_item('face_editor_lip_purse', face_editor_lip_purse)
+
+
+def update_face_editor_lip_smile(face_editor_lip_smile : float) -> None:
+	state_manager.set_item('face_editor_lip_smile', face_editor_lip_smile)
+
+
+def update_face_editor_lip_horizontal(face_editor_lip_horizontal : float) -> None:
+	state_manager.set_item('face_editor_lip_horizontal', face_editor_lip_horizontal)
+
+
+def update_face_editor_lip_vertical(face_editor_lip_vertical : float) -> None:
+	state_manager.set_item('face_editor_lip_vertical', face_editor_lip_vertical)
