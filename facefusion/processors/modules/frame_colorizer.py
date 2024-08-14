12543fe40dd9273bd94346179fadb125ecac33ac
henryruhs <info@henryruhs.com>
2024-08-14 14:56:17 +0200

    Fix stuff

--- facefusion/processors/modules/frame_colorizer.py
+++ facefusion/processors/modules/frame_colorizer.py
@@ -8,7 +8,7 @@ import numpy
 import facefusion.jobs.job_manager
 import facefusion.jobs.job_store
 import facefusion.processors.core as processors
-from facefusion import config, content_analyser, face_analyser, logger, process_manager, state_manager, wording
+from facefusion import config, content_analyser, logger, process_manager, state_manager, wording
 from facefusion.common_helper import create_int_metavar
 from facefusion.download import conditional_download_hashes, conditional_download_sources
 from facefusion.execution import create_inference_pool, has_execution_provider
