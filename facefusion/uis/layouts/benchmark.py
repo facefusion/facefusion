5f5a486016ee5ca8433684ff3bd5453bf7efd330
henryruhs <info@henryruhs.com>
2024-08-17 23:41:42 +0200

    Fix example urls

--- facefusion/uis/layouts/benchmark.py
+++ facefusion/uis/layouts/benchmark.py
@@ -9,15 +9,15 @@ def pre_check() -> bool:
 	if not state_manager.get_item('skip_download'):
 		conditional_download('.assets/examples',
 		[
-			'https://github.com/facefusion/facefusion-assets/releases-3.0.0/download/examples/source.jpg',
-			'https://github.com/facefusion/facefusion-assets/releases-3.0.0/download/examples/source.mp3',
-			'https://github.com/facefusion/facefusion-assets/releases-3.0.0/download/examples/target-240p.mp4',
-			'https://github.com/facefusion/facefusion-assets/releases-3.0.0/download/examples/target-360p.mp4',
-			'https://github.com/facefusion/facefusion-assets/releases-3.0.0/download/examples/target-540p.mp4',
-			'https://github.com/facefusion/facefusion-assets/releases-3.0.0/download/examples/target-720p.mp4',
-			'https://github.com/facefusion/facefusion-assets/releases-3.0.0/download/examples/target-1080p.mp4',
-			'https://github.com/facefusion/facefusion-assets/releases-3.0.0/download/examples/target-1440p.mp4',
-			'https://github.com/facefusion/facefusion-assets/releases-3.0.0/download/examples/target-2160p.mp4'
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0//source.jpg',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0//source.mp3',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0//target-240p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0//target-360p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0//target-540p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0//target-720p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0//target-1080p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0//target-1440p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0//target-2160p.mp4'
 		])
 		return True
 	return False
