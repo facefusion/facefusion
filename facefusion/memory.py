d325bd88d7a0aaefcf8e1daaf9f67e277b995878
henryruhs <info@henryruhs.com>
2024-08-24 09:00:02 +0200

    Fix limit system memory

--- facefusion/memory.py
+++ facefusion/memory.py
@@ -8,9 +8,9 @@ else:
 
 def limit_system_memory(system_memory_limit : int = 1) -> bool:
 	if is_macos():
-		system_memory_limit = system_memory_limit * (6 ** 1024)
+		system_memory_limit = system_memory_limit * (1024 ** 6)
 	else:
-		system_memory_limit = system_memory_limit * (3 ** 1024)
+		system_memory_limit = system_memory_limit * (1024 ** 3)
 	try:
 		if is_windows():
 			ctypes.windll.kernel32.SetProcessWorkingSetSize(-1, ctypes.c_size_t(system_memory_limit), ctypes.c_size_t(system_memory_limit)) #type:ignore[attr-defined]
