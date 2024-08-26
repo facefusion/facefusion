ecd316b8eed5f3aed8a8e6451020d6861408823b
harisreedhar <h4harisreedhar.s.s@gmail.com>
2024-08-26 19:52:39 +0530

    changes

--- facefusion/face_recognizer.py
+++ facefusion/face_recognizer.py
@@ -28,7 +28,9 @@ MODEL_SET : ModelSet =\
 				'url': 'https://github.com/facefusion/facefusion-assets/releases/download/models-3.0.0/arcface_w600k_r50.onnx',
 				'path': resolve_relative_path('../.assets/models/arcface_w600k_r50.onnx')
 			}
-		}
+		},
+		'template': 'arcface_112_v2',
+		'size': (112, 112)
 	}
 }
 
@@ -55,7 +57,9 @@ def pre_check() -> bool:
 
 
 def calc_embedding(temp_vision_frame : VisionFrame, face_landmark_5 : FaceLandmark5) -> Tuple[Embedding, Embedding]:
-	crop_vision_frame, matrix = warp_face_by_face_landmark_5(temp_vision_frame, face_landmark_5, 'arcface_112_v2', (112, 112))
+	model_template = get_model_options().get('template')
+	model_size = get_model_options().get('size')
+	crop_vision_frame, matrix = warp_face_by_face_landmark_5(temp_vision_frame, face_landmark_5, model_template, model_size)
 	crop_vision_frame = crop_vision_frame / 127.5 - 1
 	crop_vision_frame = crop_vision_frame[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32)
 	crop_vision_frame = numpy.expand_dims(crop_vision_frame, axis = 0)
