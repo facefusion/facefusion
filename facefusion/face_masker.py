e257895ad21cc117a28ef045010fe818777290cf
henryruhs <info@henryruhs.com>
2024-08-30 23:06:39 +0200

    Simplidy collect_model_downloads()

--- facefusion/face_masker.py
+++ facefusion/face_masker.py
@@ -79,12 +79,16 @@ def clear_inference_pool() -> None:
 
 
 def collect_model_downloads() -> Tuple[DownloadSet, DownloadSet]:
-	model_hashes = {}
-	model_sources = {}
-	model_hashes['face_occluder'] = MODEL_SET.get('face_occluder').get('hashes').get('face_occluder')
-	model_sources['face_occluder'] = MODEL_SET.get('face_occluder').get('sources').get('face_occluder')
-	model_hashes['face_parser'] = MODEL_SET.get('face_parser').get('hashes').get('face_parser')
-	model_sources['face_parser'] = MODEL_SET.get('face_parser').get('sources').get('face_parser')
+	model_hashes =\
+	{
+		'face_occluder': MODEL_SET.get('face_occluder').get('hashes').get('face_occluder'),
+		'face_parser': MODEL_SET.get('face_parser').get('hashes').get('face_parser')
+	}
+	model_sources =\
+	{
+		'face_occluder': MODEL_SET.get('face_occluder').get('sources').get('face_occluder'),
+		'face_parser': MODEL_SET.get('face_parser').get('sources').get('face_parser')
+	}
 	return model_hashes, model_sources
 
 
