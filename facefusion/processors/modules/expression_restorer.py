1d36e85b853d588ccc27db9be7362e3f777fa6da
harisreedhar <h4harisreedhar.s.s@gmail.com>
2024-09-19 12:13:00 +0530

    limit expression restorer

--- facefusion/processors/modules/expression_restorer.py
+++ facefusion/processors/modules/expression_restorer.py
@@ -137,7 +137,7 @@ def post_process() -> None:
 def restore_expression(source_vision_frame : VisionFrame, target_face : Face, temp_vision_frame : VisionFrame) -> VisionFrame:
 	model_template = get_model_options().get('template')
 	model_size = get_model_options().get('size')
-	expression_restorer_factor = float(numpy.interp(float(state_manager.get_item('expression_restorer_factor')), [ 0, 200 ], [ 0, 2 ]))
+	expression_restorer_factor = float(numpy.interp(float(state_manager.get_item('expression_restorer_factor')), [ 0, 120 ], [ 0, 1.2 ]))
 	source_vision_frame = cv2.resize(source_vision_frame, temp_vision_frame.shape[:2][::-1])
 	source_crop_vision_frame, _ = warp_face_by_face_landmark_5(source_vision_frame, target_face.landmark_set.get('5/68'), model_template, model_size)
 	target_crop_vision_frame, affine_matrix = warp_face_by_face_landmark_5(temp_vision_frame, target_face.landmark_set.get('5/68'), model_template, model_size)
