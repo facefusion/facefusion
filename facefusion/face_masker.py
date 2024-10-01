904de93565c94f28d70e494d503ae4f92afed6c8
henryruhs <info@henryruhs.com>
2024-10-01 22:34:42 +0200

    Rename resnet_34 to bisenet_resnet_34

--- facefusion/face_masker.py
+++ facefusion/face_masker.py
@@ -39,16 +39,16 @@ MODEL_SET : ModelSet =\
 		{
 			'face_parser':
 			{
-				'url': 'https://github.com/facefusion/facefusion-assets/releases/download/models-3.0.0/resnet_34.hash',
-				'path': resolve_relative_path('../.assets/models/resnet_34.hash')
+				'url': 'https://github.com/facefusion/facefusion-assets/releases/download/models-3.0.0/bisenet_resnet_34.hash',
+				'path': resolve_relative_path('../.assets/models/bisenet_resnet_34.hash')
 			}
 		},
 		'sources':
 		{
 			'face_parser':
 			{
-				'url': 'https://github.com/facefusion/facefusion-assets/releases/download/models-3.0.0/resnet_34.onnx',
-				'path': resolve_relative_path('../.assets/models/resnet_34.onnx')
+				'url': 'https://github.com/facefusion/facefusion-assets/releases/download/models-3.0.0/bisenet_resnet_34.onnx',
+				'path': resolve_relative_path('../.assets/models/bisenet_resnet_34.onnx')
 			}
 		},
 		'size': (512, 512)
