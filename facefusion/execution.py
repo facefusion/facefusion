f89398d68669a1897fa1f3a86f5903290b10c88d
henryruhs <info@henryruhs.com>
2024-11-03 23:30:45 +0100

    Update onnxruntime (second try)

--- facefusion/execution.py
+++ facefusion/execution.py
@@ -37,7 +37,7 @@ def create_execution_providers(execution_device_id : str, execution_provider_key
 			execution_providers.append((execution_provider_set.get(execution_provider_key),
 			{
 				'device_id': execution_device_id,
-				'cudnn_conv_algo_search': 'EXHAUSTIVE' if use_exhaustive() else 'DEFAULT'
+				'cudnn_conv_algo_search': 'EXHAUSTIVE' if use_exhaustive() else 'HEURISTIC'
 			}))
 		if execution_provider_key == 'tensorrt':
 			execution_providers.append((execution_provider_set.get(execution_provider_key),
