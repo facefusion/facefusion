0c497f2953b09392b05f6c90df8ec8f598d2683a
bruce-yang <yangfuchao2010@gmail.com>
2024-12-18 17:45:52 +0800

    Update content_analyser.py

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -111,8 +111,7 @@ def analyse_video(video_path : str, start_frame : int, end_frame : int) -> bool:
 	#frame_range = range(start_frame or 0, end_frame or video_frame_total)
 	#rate = 0.0
 	#counter = 0
-
-	#with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = state_manager.get_item('log_level') in [ 'warn', 'error' ]) as progress:
+    #with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = state_manager.get_item('log_level') in [ 'warn', 'error' ]) as progress:
 	#	for frame_number in frame_range:
 	#		if frame_number % int(video_fps) == 0:
 	#			frame = get_video_frame(video_path, frame_number)
