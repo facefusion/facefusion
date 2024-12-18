7a0c7cd63a19748bc6e04f5be3c40084f557f6eb
bruce-yang <yangfuchao2010@gmail.com>
2024-12-18 16:57:52 +0800

    Update content_analyser.py

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -106,19 +106,21 @@ def analyse_image(image_path : str) -> bool:
 
 @lru_cache(maxsize = None)
 def analyse_video(video_path : str, start_frame : int, end_frame : int) -> bool:
-	video_frame_total = count_video_frame_total(video_path)
-	video_fps = detect_video_fps(video_path)
-	frame_range = range(start_frame or 0, end_frame or video_frame_total)
-	rate = 0.0
-	counter = 0
+	#video_frame_total = count_video_frame_total(video_path)
+	#video_fps = detect_video_fps(video_path)
+	#frame_range = range(start_frame or 0, end_frame or video_frame_total)
+	#rate = 0.0
+	#counter = 0
 
-	with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = state_manager.get_item('log_level') in [ 'warn', 'error' ]) as progress:
-		for frame_number in frame_range:
-			if frame_number % int(video_fps) == 0:
-				frame = get_video_frame(video_path, frame_number)
-				if analyse_frame(frame):
-					counter += 1
-			rate = counter * int(video_fps) / len(frame_range) * 100
-			progress.update()
-			progress.set_postfix(rate = rate)
-	return rate > RATE_LIMIT
+	#with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = state_manager.get_item('log_level') in [ 'warn', 'error' ]) as progress:
+	#	for frame_number in frame_range:
+	#		if frame_number % int(video_fps) == 0:
+	#			frame = get_video_frame(video_path, frame_number)
+	#			if analyse_frame(frame):
+	#				counter += 1
+	#		rate = counter * int(video_fps) / len(frame_range) * 100
+	#		progress.update()
+	#		progress.set_postfix(rate = rate)
+	#return rate > RATE_LIMIT
+	return false;
+	
