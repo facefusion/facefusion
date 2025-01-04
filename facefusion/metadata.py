afebbf15782e9c70c15ca88ecab275ac25ff125d
henryruhs <info@henryruhs.com>
2025-01-04 13:23:00 +0100

    Introduce NEXT

--- facefusion/metadata.py
+++ facefusion/metadata.py
@@ -4,7 +4,7 @@ METADATA =\
 {
 	'name': 'FaceFusion',
 	'description': 'Industry leading face manipulation platform',
-	'version': '3.1.1',
+	'version': 'NEXT',
 	'license': 'MIT',
 	'author': 'Henry Ruhs',
 	'url': 'https://facefusion.io'
