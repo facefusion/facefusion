36a861fd3ff6d223831115bd92a9a02d5d58abfa
henryruhs <info@henryruhs.com>
2025-01-13 11:46:56 +0100

    Fix set audio quality

--- tests/test_curl_builder.py
+++ tests/test_curl_builder.py
@@ -1,17 +1,18 @@
 from shutil import which
 
-from facefusion import curl_builder, metadata
+from facefusion import metadata
+from facefusion.curl_builder import chain, head, run
 
 
 def test_run() -> None:
 	user_agent = metadata.get('name') + '/' + metadata.get('version')
 
-	assert curl_builder.run([]) == [ which('curl'), '--user-agent', user_agent, '--insecure', '--location', '--silent' ]
+	assert run([]) == [ which('curl'), '--user-agent', user_agent, '--insecure', '--location', '--silent' ]
 
 
 def test_chain() -> None:
-	commands = curl_builder.chain(
-		curl_builder.head(metadata.get('url'))
+	commands = chain(
+		head(metadata.get('url'))
 	)
 
 	assert commands == [ '-I', metadata.get('url') ]
