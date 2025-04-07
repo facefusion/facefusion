e6f1f6f5a2a32cccd76fd7e835d8d8ee48c96df7
TheKhosa <126021122+TheKhosa@users.noreply.github.com>
2025-04-07 14:40:03 +0100

    Update default.py
    removed localhost only all network accessible

--- facefusion/uis/layouts/default.py
+++ facefusion/uis/layouts/default.py
@@ -116,4 +116,9 @@ def listen() -> None:
 
 
 def run(ui : gradio.Blocks) -> None:
-	ui.launch(favicon_path = 'facefusion.ico', inbrowser = state_manager.get_item('open_browser'))
+    ui.launch(
+        favicon_path = 'facefusion.ico', 
+        inbrowser = state_manager.get_item('open_browser'),
+        server_name = '0.0.0.0',
+        server_port = 7860
+    )
