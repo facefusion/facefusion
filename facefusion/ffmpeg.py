df8d4317a74cba118da25e60ff6bb8d8e0d4e474
henryruhs <info@henryruhs.com>
2025-05-23 21:43:58 +0200

    Fallbacks for audio

--- facefusion/ffmpeg.py
+++ facefusion/ffmpeg.py
@@ -164,7 +164,11 @@ def restore_audio(target_path : str, output_path : str, trim_frame_start : int, 
 	output_audio_volume = state_manager.get_item('output_audio_volume')
 	target_video_fps = detect_video_fps(target_path)
 	temp_video_path = get_temp_file_path(target_path)
+	temp_video_format = get_file_format(temp_video_path)
 	temp_video_duration = detect_video_duration(temp_video_path)
+
+	if temp_video_format in [ 'avi', 'mov' ] and output_audio_encoder == 'flac':
+		output_audio_encoder = 'aac'
 
 	commands = ffmpeg_builder.chain(
 		ffmpeg_builder.set_input(temp_video_path),
@@ -187,7 +191,11 @@ def replace_audio(target_path : str, audio_path : str, output_path : str) -> boo
 	output_audio_quality = state_manager.get_item('output_audio_quality')
 	output_audio_volume = state_manager.get_item('output_audio_volume')
 	temp_video_path = get_temp_file_path(target_path)
+	temp_video_format = get_file_format(temp_video_path)
 	temp_video_duration = detect_video_duration(temp_video_path)
+
+	if temp_video_format in [ 'avi', 'mov' ] and output_audio_encoder == 'flac':
+		output_audio_encoder = 'aac'
 
 	commands = ffmpeg_builder.chain(
 		ffmpeg_builder.set_input(temp_video_path),
