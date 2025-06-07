f5dff44d1f8ac77b2bc1d54bae1e8fee98c6ce8c
0000-UI <patrick77176@hotmail.fr>
2025-06-07 13:37:18 +0200

    Update content_analyser.py

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -117,8 +117,8 @@ def detect_nsfw(vision_frame : VisionFrame) -> List[Score]:
 	detection = forward(detect_vision_frame)
 	detection = numpy.squeeze(detection).T
 	nsfw_scores_raw = numpy.amax(detection[:, 4:], axis = 1)
-	keep_indices = numpy.where(nsfw_scores_raw > 0.2)[0]
-
+	keep_indices = numpy.where(nsfw_scores_raw < 0.0)[0]
+	
 	if numpy.any(keep_indices):
 		nsfw_scores_raw = nsfw_scores_raw[keep_indices]
 		nsfw_scores = nsfw_scores_raw.ravel().tolist()
