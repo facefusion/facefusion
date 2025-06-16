bf651f5420a4bc1bcfc3c2500af40ad7c0cb672d
henryruhs <info@henryruhs.com>
2025-06-16 12:03:20 +0200

    Fix import

--- facefusion/uis/layouts/benchmark.py
+++ facefusion/uis/layouts/benchmark.py
@@ -2,10 +2,7 @@ import gradio
 
 from facefusion import state_manager
 from facefusion.benchmarker import pre_check as benchmarker_pre_check
-from facefusion.uis.components import (about, age_modifier_options, benchmark, benchmark_options, deep_swapper_options,
-										download, execution, execution_queue_count, execution_thread_count, expression_restorer_options,
-										face_debugger_options, face_editor_options, face_enhancer_options, face_swapper_options,
-										frame_colorizer_options, frame_enhancer_options, lip_syncer_options, memory, processors)
+from facefusion.uis.components import about, age_modifier_options, benchmark, benchmark_options, deep_swapper_options, download, execution, execution_queue_count, execution_thread_count, expression_restorer_options, face_debugger_options, face_editor_options, face_enhancer_options, face_swapper_options, frame_colorizer_options, frame_enhancer_options, lip_syncer_options, memory, processors
 
 
 def pre_check() -> bool:
