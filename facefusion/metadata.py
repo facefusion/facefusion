7b8bea4e0a0f883e46e69eee2b37c5ffd0a818f8
henryruhs <info@henryruhs.com>
2025-07-10 15:22:41 +0200

    Make HyperSwap the default

--- facefusion/metadata.py
+++ facefusion/metadata.py
@@ -4,7 +4,7 @@ METADATA =\
 {
 	'name': 'FaceFusion',
 	'description': 'Industry leading face manipulation platform',
-	'version': '3.3.1',
+	'version': '3.3.2',
 	'license': 'OpenRAIL-AS',
 	'author': 'Henry Ruhs',
 	'url': 'https://facefusion.io'
