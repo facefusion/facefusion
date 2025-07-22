64a27e33326a2c8b2d7031315200d08851df458e
henryruhs <info@henryruhs.com>
2025-07-22 08:43:09 +0200

    Introduce new concept of pure target frames

--- facefusion/uis/components/preview.py
+++ facefusion/uis/components/preview.py
@@ -10,11 +10,10 @@ from facefusion.audio import create_empty_audio_frame, get_audio_frame
 from facefusion.common_helper import get_first
 from facefusion.content_analyser import analyse_frame
 from facefusion.core import conditional_append_reference_faces
-from facefusion.face_analyser import get_average_face, get_many_faces
 from facefusion.face_store import clear_reference_faces, clear_static_faces, get_reference_faces
 from facefusion.filesystem import filter_audio_paths, is_image, is_video
 from facefusion.processors.core import get_processors_modules
-from facefusion.types import AudioFrame,  FaceSet, VisionFrame
+from facefusion.types import AudioFrame, FaceSet, VisionFrame
 from facefusion.uis.core import get_ui_component, get_ui_components, register_ui_component
 from facefusion.uis.types import ComponentOptions
 from facefusion.vision import count_video_frame_total, detect_frame_orientation, normalize_frame_color, read_static_image, read_static_images, read_video_frame, restrict_frame
