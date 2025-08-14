e455fea098a28782e473fdd1b6de878b35d13cfc
henryruhs <info@henryruhs.com>
2025-08-14 19:59:10 +0200

    Simplify output scale testing

--- tests/test_cli_output_scale.py
+++ tests/test_cli_output_scale.py
@@ -1,6 +1,5 @@
 import subprocess
 import sys
-from typing import Optional
 
 import pytest
 
@@ -8,7 +7,7 @@ from facefusion.download import conditional_download
 from facefusion.jobs.job_manager import clear_jobs, init_jobs
 from facefusion.types import Resolution, Scale
 from facefusion.vision import detect_image_resolution, detect_video_resolution
-from .helper import get_test_example_file, get_test_examples_directory, get_test_jobs_directory, get_test_output_file, is_test_output_file, prepare_test_output_directory
+from .helper import get_test_example_file, get_test_examples_directory, get_test_jobs_directory, get_test_output_file, prepare_test_output_directory
 
 
 @pytest.fixture(scope = 'module', autouse = True)
@@ -28,40 +27,31 @@ def before_each() -> None:
 	prepare_test_output_directory()
 
 
-@pytest.mark.parametrize('scale, expected_resolution, test_name',
+@pytest.mark.parametrize('output_image_scale, output_image_resolution',
 [
-	(1.0, (426, 226), "original-scale"),
-	(0.5, (212, 112), "half-scale"),
-	(2.0, (852, 452), "double-scale"),
-	(8.0, (3408, 1808), "false-scale"),
+	(0.5, (212, 112)),
+	(1.0, (426, 226)),
+	(2.0, (852, 452)),
+	(8.0, (3408, 1808))
 ])
-def test_output_image_scale(scale : Scale, expected_resolution : Optional[Resolution], test_name : str) -> None:
-	target_file_path = get_test_example_file('target-240p.jpg')
-	output_file_path = get_test_output_file(f'test-{test_name}-on-image.jpg')
-	processor = 'face_debugger' if test_name == 'false-scale' else 'frame_enhancer'
-	commands = [ sys.executable, 'facefusion.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--processors', processor, '-t', target_file_path, '-o', output_file_path, '--output-image-scale', str(scale) ]
+def test_output_image_scale(output_image_scale : Scale, output_image_resolution : Resolution) -> None:
+	output_file_path = get_test_output_file('test-output-image-scale-' + str(output_image_scale) + '.jpg')
+	commands = [ sys.executable, 'facefusion.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--processors', 'frame_enhancer', '-t', get_test_example_file('target-240p.jpg'), '-o', output_file_path, '--output-image-scale', str(output_image_scale) ]
 
 	assert subprocess.run(commands).returncode == 0
-	assert is_test_output_file(f'test-{test_name}-on-image.jpg') is True
-	assert detect_image_resolution(output_file_path) == expected_resolution
+	assert detect_image_resolution(output_file_path) == output_image_resolution
 
 
-@pytest.mark.parametrize('scale, expected_resolution, test_name',
+@pytest.mark.parametrize('output_video_scale, output_video_resolution',
 [
-	(1.0, (426, 226), 'original-scale'),
-	(0.5, (212, 112), 'half-scale'),
-	(2.0, (852, 452), 'double-scale'),
-	(8.0, (3408, 1808), 'false-scale'),
+	(0.5, (212, 112)),
+	(1.0, (426, 226)),
+	(2.0, (852, 452)),
+	(8.0, (3408, 1808))
 ])
-def test_output_video_scale(scale : Scale, expected_resolution : Optional[Resolution], test_name : str) -> None:
-	target_file_path = get_test_example_file('target-240p.mp4')
-	output_file_path = get_test_output_file(f'test-{test_name}-on-video.mp4')
-	processor = 'face_debugger' if test_name == 'false-scale' else 'frame_enhancer'
-	commands = [ sys.executable, 'facefusion.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--processors', processor, '-t', target_file_path, '-o', output_file_path, '--trim-frame-end', '1' ]
-
-	if scale:
-		commands.extend(['--output-video-scale', str(scale)])
+def test_output_video_scale(output_video_scale : Scale, output_video_resolution : Resolution) -> None:
+	output_file_path = get_test_output_file('test-output-video-scale-' + str(output_video_scale) + '.mp4')
+	commands = [ sys.executable, 'facefusion.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--processors', 'frame_enhancer', '-t', get_test_example_file('target-240p.mp4'), '-o', output_file_path, '--trim-frame-end', '1', '--output-video-scale', str(output_video_scale) ]
 
 	assert subprocess.run(commands).returncode == 0
-	assert is_test_output_file(f'test-{test_name}-on-video.mp4') is True
-	assert detect_video_resolution(output_file_path) == expected_resolution
+	assert detect_video_resolution(output_file_path) == output_video_resolution
