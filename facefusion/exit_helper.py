9b52a857c5120a45b14f5c467b57e9d9ce9745ba
henryruhs <info@henryruhs.com>
2025-09-06 14:23:55 +0200

    Fix hard exit to work without signals

--- facefusion/exit_helper.py
+++ facefusion/exit_helper.py
@@ -1,5 +1,5 @@
+import os
 import signal
-import sys
 from time import sleep
 from types import FrameType
 
@@ -9,8 +9,7 @@ from facefusion.types import ErrorCode
 
 
 def hard_exit(error_code : ErrorCode) -> None:
-	signal.signal(signal.SIGINT, signal.SIG_IGN)
-	sys.exit(error_code)
+	os._exit(error_code)
 
 
 def signal_exit(signum : int, frame : FrameType) -> None:
@@ -18,9 +17,13 @@ def signal_exit(signum : int, frame : FrameType) -> None:
 
 
 def graceful_exit(error_code : ErrorCode) -> None:
+	signal.signal(signal.SIGINT, signal.SIG_IGN)
 	process_manager.stop()
+
 	while process_manager.is_processing():
 		sleep(0.5)
+
 	if state_manager.get_item('target_path'):
 		clear_temp_directory(state_manager.get_item('target_path'))
+
 	hard_exit(error_code)
