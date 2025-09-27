9e3ca35b708ea0974e90e43e38a1f30a69598704
henryruhs <info@henryruhs.com>
2025-09-27 21:00:38 +0200

    Update dependencies

--- facefusion/installer.py
+++ facefusion/installer.py
@@ -12,15 +12,15 @@ from facefusion.common_helper import is_linux, is_windows
 
 ONNXRUNTIME_SET =\
 {
-	'default': ('onnxruntime', '1.22.1')
+	'default': ('onnxruntime', '1.23.0')
 }
 if is_windows() or is_linux():
-	ONNXRUNTIME_SET['cuda'] = ('onnxruntime-gpu', '1.22.0')
+	ONNXRUNTIME_SET['cuda'] = ('onnxruntime-gpu', '1.23.0')
 	ONNXRUNTIME_SET['openvino'] = ('onnxruntime-openvino', '1.22.0')
 if is_windows():
 	ONNXRUNTIME_SET['directml'] = ('onnxruntime-directml', '1.17.3')
 if is_linux():
-	ONNXRUNTIME_SET['rocm'] = ('onnxruntime-rocm', '1.21.0')
+	ONNXRUNTIME_SET['rocm'] = ('onnxruntime-rocm', '1.22.1')
 
 
 def cli() -> None:
