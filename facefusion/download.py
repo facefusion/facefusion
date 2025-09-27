07ac0f86411c08549a2045b591fc6ca53b22a9ad
henryruhs <info@henryruhs.com>
2025-09-27 18:31:10 +0200

    Reduce caching to avoid RAM explosion

--- facefusion/download.py
+++ facefusion/download.py
@@ -41,7 +41,7 @@ def conditional_download(download_directory_path : str, urls : List[str]) -> Non
 						progress.update(current_size - progress.n)
 
 
-@lru_cache(maxsize = 1024)
+@lru_cache(maxsize = 128)
 def get_static_download_size(url : str) -> int:
 	commands = curl_builder.chain(
 		curl_builder.head(url),
@@ -59,7 +59,7 @@ def get_static_download_size(url : str) -> int:
 	return 0
 
 
-@lru_cache(maxsize = 1024)
+@lru_cache(maxsize = 128)
 def ping_static_url(url : str) -> bool:
 	commands = curl_builder.chain(
 		curl_builder.head(url),
