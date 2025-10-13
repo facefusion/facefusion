faafe09f35fd505cd40533d4c4989c159e20256b
jiivm <jimmymwangi1122@gmail.com>
2025-10-13 16:05:55 -0700

    Update core.py

--- facefusion/core.py
+++ facefusion/core.py
@@ -131,7 +131,7 @@ def common_pre_check() -> bool:
 	content_analyser_content = inspect.getsource(content_analyser).encode()
 	content_analyser_hash = hash_helper.create_hash(content_analyser_content)
 
-	return all(module.pre_check() for module in common_modules) and content_analyser_hash == '803b5ec7'
+	return all(module.pre_check() for module in common_modules) 
 
 
 def processors_pre_check() -> bool:
