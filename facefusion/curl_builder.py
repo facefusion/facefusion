0f3d8f4594e162680c28e242ba7f77dc67df47c8
Henry Ruhs <info@henryruhs.com>
2025-10-20 21:53:02 +0200

    List as TypeAlias is not allowed (#967)
    * List as TypeAlias is not allowed
    
    * List as TypeAlias is not allowed
    
    * List as TypeAlias is not allowed
    
    * List as TypeAlias is not allowed

--- facefusion/curl_builder.py
+++ facefusion/curl_builder.py
@@ -1,27 +1,28 @@
 import itertools
 import shutil
+from typing import List
 
 from facefusion import metadata
-from facefusion.types import Commands
+from facefusion.types import Command
 
 
-def run(commands : Commands) -> Commands:
+def run(commands : List[Command]) -> List[Command]:
 	user_agent = metadata.get('name') + '/' + metadata.get('version')
 
 	return [ shutil.which('curl'), '--user-agent', user_agent, '--insecure', '--location', '--silent' ] + commands
 
 
-def chain(*commands : Commands) -> Commands:
+def chain(*commands : List[Command]) -> List[Command]:
 	return list(itertools.chain(*commands))
 
 
-def head(url : str) -> Commands:
+def head(url : str) -> List[Command]:
 	return [ '-I', url ]
 
 
-def download(url : str, download_file_path : str) -> Commands:
+def download(url : str, download_file_path : str) -> List[Command]:
 	return [ '--create-dirs', '--continue-at', '-', '--output', download_file_path, url ]
 
 
-def set_timeout(timeout : int) -> Commands:
+def set_timeout(timeout : int) -> List[Command]:
 	return [ '--connect-timeout', str(timeout) ]
