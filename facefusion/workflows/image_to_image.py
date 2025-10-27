abb6c2393dc1ad69c07450b2c944c2d4a876b860
harisreedhar <h4harisreedhar.s.s@gmail.com>
2025-10-27 14:29:53 +0530

    fix lint

--- facefusion/workflows/image_to_image.py
+++ facefusion/workflows/image_to_image.py
@@ -9,7 +9,7 @@ from facefusion.processors.core import get_processors_modules
 from facefusion.temp_helper import clear_temp_directory, create_temp_directory, get_temp_file_path
 from facefusion.time_helper import calculate_end_time
 from facefusion.types import ErrorCode
-from facefusion.vision import extract_vision_mask, detect_image_resolution, merge_vision_mask, pack_resolution, read_static_image, read_static_images, restrict_image_resolution, scale_resolution, write_image
+from facefusion.vision import detect_image_resolution, extract_vision_mask, merge_vision_mask, pack_resolution, read_static_image, read_static_images, restrict_image_resolution, scale_resolution, write_image
 from facefusion.workflows.core import is_process_stopping
 
 
