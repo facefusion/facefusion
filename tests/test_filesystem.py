3c2c03701b176184d37f68e6c1cfe5c0c22a607d
harisreedhar <h4harisreedhar.s.s@gmail.com>
2025-11-14 16:54:21 +0530

    fix

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -3,7 +3,7 @@ import os.path
 import pytest
 
 from facefusion.download import conditional_download
-from facefusion.filesystem import create_directory, filter_audio_paths, filter_image_paths, get_file_extension, get_file_format, get_file_size, has_audio, has_image, has_video, in_directory, is_audio, is_directory, is_file, is_image, is_video, remove_directory, resolve_file_paths, same_file_extension
+from facefusion.filesystem import create_directory, filter_audio_paths, filter_image_paths, get_file_extension, get_file_format, get_file_size, has_audio, has_image, has_video, in_directory, is_audio, is_directory, is_file, is_image, is_video, remove_directory, resolve_file_paths
 from .helper import get_test_example_file, get_test_examples_directory, get_test_outputs_directory
 
 
@@ -33,12 +33,6 @@ def test_get_file_format() -> None:
 	assert get_file_format('source.jpeg') == 'jpeg'
 	assert get_file_format('source.mp3') == 'mp3'
 	assert get_file_format('invalid') is None
-
-
-def test_same_file_extension() -> None:
-	assert same_file_extension('source.jpg', 'source.jpg') is True
-	assert same_file_extension('source.jpg', 'source.mp3') is False
-	assert same_file_extension('invalid', 'invalid') is False
 
 
 def test_is_file() -> None:
