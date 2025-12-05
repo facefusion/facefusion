80628f2ed404e54dd76e4cd57e182f6e8f512032
harisreedhar <h4harisreedhar.s.s@gmail.com>
2025-12-05 12:53:27 +0530

    changes
    restructure conditional methods to a fall-through pattern
    
    common process_temp_frame for all workflow

--- facefusion/workflows/core.py
+++ facefusion/workflows/core.py
@@ -1,6 +1,13 @@
+import numpy
+
 from facefusion import logger, process_manager, state_manager, translator
+from facefusion.audio import create_empty_audio_frame, get_audio_frame, get_voice_frame
+from facefusion.common_helper import get_first
+from facefusion.filesystem import filter_audio_paths
+from facefusion.processors.core import get_processors_modules
 from facefusion.temp_helper import clear_temp_directory, create_temp_directory
-from facefusion.types import ErrorCode
+from facefusion.types import AudioFrame, ErrorCode, VisionFrame
+from facefusion.vision import conditional_merge_vision_mask, extract_vision_mask, read_static_image, read_static_images, read_static_video_frame, restrict_video_fps, write_image
 
 
 def is_process_stopping() -> bool:
@@ -20,3 +27,64 @@ def clear() -> ErrorCode:
 	clear_temp_directory(state_manager.get_temp_path(), state_manager.get_item('output_path'))
 	logger.debug(translator.get('clearing_temp'), __name__)
 	return 0
+
+
+def conditional_get_source_audio_frame(frame_number : int) -> AudioFrame:
+	if state_manager.get_item('workflow') in [ 'audio-to-image', 'image-to-video' ]:
+		source_audio_path = get_first(filter_audio_paths(state_manager.get_item('source_paths')))
+		output_video_fps = state_manager.get_item('output_video_fps')
+
+		if state_manager.get_item('workflow') == 'image-to-video':
+			output_video_fps = restrict_video_fps(state_manager.get_item('target_path'), output_video_fps)
+		source_audio_frame = get_audio_frame(source_audio_path, output_video_fps, frame_number)
+
+		if numpy.any(source_audio_frame):
+			return source_audio_frame
+
+	return create_empty_audio_frame()
+
+
+def conditional_get_source_voice_frame(frame_number: int) -> AudioFrame:
+	if state_manager.get_item('workflow') in [ 'audio-to-image', 'image-to-video' ]:
+		source_audio_path = get_first(filter_audio_paths(state_manager.get_item('source_paths')))
+		output_video_fps = state_manager.get_item('output_video_fps')
+
+		if state_manager.get_item('workflow') == 'image-to-video':
+			output_video_fps = restrict_video_fps(state_manager.get_item('target_path'), output_video_fps)
+		source_voice_frame = get_voice_frame(source_audio_path, output_video_fps, frame_number)
+
+		if numpy.any(source_voice_frame):
+			return source_voice_frame
+
+	return create_empty_audio_frame()
+
+
+def conditional_get_reference_vision_frame() -> VisionFrame:
+	if state_manager.get_item('workflow') == 'image-to-video':
+		return read_static_video_frame(state_manager.get_item('target_path'), state_manager.get_item('reference_frame_number'))
+	return read_static_image(state_manager.get_item('target_path'))
+
+
+def process_temp_frame(temp_frame_path : str, frame_number : int) -> bool:
+	reference_vision_frame = conditional_get_reference_vision_frame()
+	source_vision_frames = read_static_images(state_manager.get_item('source_paths'))
+	target_vision_frame = read_static_image(temp_frame_path, 'rgba')
+	source_audio_frame = conditional_get_source_audio_frame(frame_number)
+	source_voice_frame = conditional_get_source_voice_frame(frame_number)
+	temp_vision_frame = target_vision_frame.copy()
+	temp_vision_mask = extract_vision_mask(temp_vision_frame)
+
+	for processor_module in get_processors_modules(state_manager.get_item('processors')):
+		temp_vision_frame, temp_vision_mask = processor_module.process_frame(
+		{
+			'reference_vision_frame': reference_vision_frame,
+			'source_vision_frames': source_vision_frames,
+			'source_audio_frame': source_audio_frame,
+			'source_voice_frame': source_voice_frame,
+			'target_vision_frame': target_vision_frame[:, :, :3],
+			'temp_vision_frame': temp_vision_frame[:, :, :3],
+			'temp_vision_mask': temp_vision_mask
+		})
+
+	temp_vision_frame = conditional_merge_vision_mask(temp_vision_frame, temp_vision_mask)
+	return write_image(temp_frame_path, temp_vision_frame)
