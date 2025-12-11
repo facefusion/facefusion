c58aa52eb2a24f39470aa162bda6c577c80d0082
Henry Ruhs <info@henryruhs.com>
2025-12-11 12:00:59 +0100

    feat/ping-endpoint (#1001)
    * api: add WebSocket /ping endpoint and update session guard to support WebSocket subprotocol auth; add tests (test_api_ping.py)
    
    * Initial websocket support using ping
    
    * Initial websocket support using ping
    
    * Initial websocket support using ping
    
    * Combine imports

--- facefusion/apis/core.py
+++ facefusion/apis/core.py
@@ -1,28 +1,25 @@
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
-from starlette.routing import Route
+from starlette.routing import Route, WebSocketRoute
 
-from facefusion.apis.session import create_session
-from facefusion.apis.session import create_session_guard
-from facefusion.apis.session import destroy_session
-from facefusion.apis.session import get_session
-from facefusion.apis.session import refresh_session
-from facefusion.apis.state import get_state
-from facefusion.apis.state import set_state
+from facefusion.apis.ping import websocket_ping
+from facefusion.apis.session import create_session, create_session_guard, destroy_session, get_session, refresh_session
+from facefusion.apis.state import get_state, set_state
 
 
 def create_api() -> Starlette:
 	session_guard = Middleware(create_session_guard)
 	routes =\
-	[
-		Route('/session', create_session, methods = [ 'POST' ]),
-		Route('/session', get_session, methods = [ 'GET' ], middleware = [ session_guard ]),
-		Route('/session', refresh_session, methods = [ 'PUT' ]),
-		Route('/session', destroy_session, methods = [ 'DELETE' ], middleware = [ session_guard ]),
-		Route('/state', get_state, methods = [ 'GET' ], middleware = [ session_guard ]),
-		Route('/state', set_state, methods = [ 'PUT' ], middleware = [ session_guard ])
-	]
+		[
+			Route('/session', create_session, methods = [ 'POST' ]),
+			Route('/session', get_session, methods = [ 'GET' ], middleware = [ session_guard ]),
+			Route('/session', refresh_session, methods = [ 'PUT' ]),
+			Route('/session', destroy_session, methods = [ 'DELETE' ], middleware = [ session_guard ]),
+			Route('/state', get_state, methods = [ 'GET' ], middleware = [ session_guard ]),
+			Route('/state', set_state, methods = [ 'PUT' ], middleware = [ session_guard ]),
+			WebSocketRoute('/ping', websocket_ping, middleware = [ session_guard ])
+		]
 
 	api = Starlette(routes = routes)
 	api.add_middleware(CORSMiddleware, allow_origins = [ '*' ], allow_methods = [ '*' ], allow_headers = [ '*' ])
