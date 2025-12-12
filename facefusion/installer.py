533f5ca70d1aae65b745c08100482579e06e8db1
henryruhs <info@henryruhs.com>
2025-12-12 08:13:20 +0100

    Make --force-install optional

--- facefusion/installer.py
+++ facefusion/installer.py
@@ -13,6 +13,7 @@ from facefusion.common_helper import is_linux, is_windows
 LOCALS =\
 {
 	'install_dependency': 'install the {dependency} package',
+	'force_reinstall': 'force reinstall of packages',
 	'skip_conda': 'skip the conda environment check',
 	'conda_not_activated': 'conda is not activated'
 }
@@ -34,6 +35,7 @@ def cli() -> None:
 	signal.signal(signal.SIGINT, signal_exit)
 	program = ArgumentParser(formatter_class = partial(HelpFormatter, max_help_position = 50))
 	program.add_argument('--onnxruntime', help = LOCALS.get('install_dependency').format(dependency = 'onnxruntime'), choices = ONNXRUNTIME_SET.keys(), required = True)
+	program.add_argument('--force-reinstall', help = LOCALS.get('force_reinstall'), action = 'store_true')
 	program.add_argument('--skip-conda', help = LOCALS.get('skip_conda'), action = 'store_true')
 	program.add_argument('-v', '--version', version = metadata.get('name') + ' ' + metadata.get('version'), action = 'version')
 	run(program)
@@ -46,7 +48,10 @@ def signal_exit(signum : int, frame : FrameType) -> None:
 def run(program : ArgumentParser) -> None:
 	args = program.parse_args()
 	has_conda = 'CONDA_PREFIX' in os.environ
-	requirements = []
+	commands = [ shutil.which('pip'), 'install' ]
+
+	if args.force_reinstall:
+		commands.append('--force-reinstall')
 
 	if not args.skip_conda and not has_conda:
 		sys.stdout.write(LOCALS.get('conda_not_activated') + os.linesep)
@@ -57,7 +62,7 @@ def run(program : ArgumentParser) -> None:
 		for line in file.readlines():
 			__line__ = line.strip()
 			if not __line__.startswith('onnxruntime'):
-				requirements.append(__line__)
+				commands.append(__line__)
 
 	if args.onnxruntime == 'rocm':
 		onnxruntime_name, onnxruntime_version, rocm_version = ONNXRUNTIME_SET.get(args.onnxruntime) #type:ignore[misc]
@@ -66,12 +71,12 @@ def run(program : ArgumentParser) -> None:
 		if python_id in [ 'cp310', 'cp312' ]:
 			wheel_name = onnxruntime_name + '-' + onnxruntime_version + '-' + python_id + '-' + python_id + '-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl'
 			wheel_url = 'https://repo.radeon.com/rocm/manylinux/rocm-rel-' + rocm_version + '/' + wheel_name
-			requirements.append(wheel_url)
+			commands.append(wheel_url)
 	else:
 		onnxruntime_name, onnxruntime_version = ONNXRUNTIME_SET.get(args.onnxruntime)
-		requirements.append(onnxruntime_name + '==' + onnxruntime_version)
+		commands.append(onnxruntime_name + '==' + onnxruntime_version)
 
-	subprocess.call([ shutil.which('pip'), 'install' ] + requirements + [ '--force-reinstall' ])
+	subprocess.call(commands)
 
 	if args.onnxruntime == 'cuda' and has_conda:
 		library_paths = []
@@ -102,4 +107,3 @@ def run(program : ArgumentParser) -> None:
 			library_paths = list(dict.fromkeys([ library_path for library_path in library_paths if os.path.exists(library_path) ]))
 
 			subprocess.call([ shutil.which('conda'), 'env', 'config', 'vars', 'set', 'PATH=' + os.pathsep.join(library_paths) ])
-
