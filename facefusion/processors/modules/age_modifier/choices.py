cf9bde3daceca123958f572cb8acc4e81734c672
harisreedhar <h4harisreedhar.s.s@gmail.com>
2025-12-24 15:51:35 +0530

    add fran model

--- facefusion/processors/modules/age_modifier/choices.py
+++ facefusion/processors/modules/age_modifier/choices.py
@@ -3,6 +3,6 @@ from typing import List, Sequence
 from facefusion.common_helper import create_int_range
 from facefusion.processors.modules.age_modifier.types import AgeModifierModel
 
-age_modifier_models : List[AgeModifierModel] = [ 'styleganex_age' ]
+age_modifier_models : List[AgeModifierModel] = [ 'fran', 'styleganex_age' ]
 
 age_modifier_direction_range : Sequence[int] = create_int_range(-100, 100, 1)
