f84b2cfc0007f199e17bbeec7d2c3a7f1e625b71
harisreedhar <h4harisreedhar.s.s@gmail.com>
2026-01-12 18:00:43 +0530

    asset store update

--- tests/test_api_state.py
+++ tests/test_api_state.py
@@ -1,3 +1,4 @@
+import subprocess
 from typing import Iterator
 
 import pytest
@@ -6,6 +7,18 @@ from starlette.testclient import TestClient
 from facefusion import args_store, metadata, session_manager, state_manager
 from facefusion.apis import asset_store
 from facefusion.apis.core import create_api
+from facefusion.download import conditional_download
+from .helper import get_test_example_file, get_test_examples_directory
+
+
+@pytest.fixture(scope = 'module', autouse = True)
+def before_all() -> None:
+	conditional_download(get_test_examples_directory(),
+	[
+		'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0/source.jpg',
+		'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0/target-240p.mp4'
+	])
+	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('target-240p.mp4'), '-vframes', '1', get_test_example_file('target-240p.jpg') ])
 
 
 @pytest.fixture(scope = 'module')
@@ -85,10 +98,23 @@ def test_set_state(test_client : TestClient) -> None:
 
 
 def test_select_source_assets(test_client : TestClient) -> None:
+	create_session_response = test_client.post('/session', json =
+	{
+		'client_version': metadata.get('version')
+	})
+
+	create_session_body = create_session_response.json()
+	access_token = create_session_body.get('access_token')
+	session_id = session_manager.find_session_id(access_token)
+	source_paths =\
+	[
+		get_test_example_file('source.jpg'),
+		get_test_example_file('source.jpg')
+	]
 	asset_ids =\
 	[
-		asset_store.register_asset('/path/to/source1.jpg'),
-		asset_store.register_asset('/path/to/source2.jpg')
+		asset_store.create_asset(session_id, 'source', source_paths[0]).get('id'),
+		asset_store.create_asset(session_id, 'source', source_paths[1]).get('id')
 	]
 
 	select_response = test_client.put('/state?action=select&type=source', json =
@@ -98,18 +124,12 @@ def test_select_source_assets(test_client : TestClient) -> None:
 
 	assert select_response.status_code == 401
 
-	create_session_response = test_client.post('/session', json =
-	{
-		'client_version': metadata.get('version')
-	})
-	create_session_body = create_session_response.json()
-
 	select_response = test_client.put('/state?action=select&type=source', json =
 	{
 		'asset_ids': 'invalid'
 	}, headers =
 	{
-		'Authorization': 'Bearer ' + create_session_body.get('access_token')
+		'Authorization': 'Bearer ' + access_token
 	})
 
 	assert select_response.status_code == 404
@@ -119,36 +139,38 @@ def test_select_source_assets(test_client : TestClient) -> None:
 		'asset_ids': asset_ids
 	}, headers =
 	{
-		'Authorization': 'Bearer ' + create_session_body.get('access_token')
+		'Authorization': 'Bearer ' + access_token
 	})
 	select_body = select_response.json()
 
-	assert select_body.get('source_paths') == [ '/path/to/source1.jpg', '/path/to/source2.jpg' ]
+	assert select_body.get('source_paths') == source_paths
 	assert select_response.status_code == 200
 
 
 def test_select_target_assets(test_client : TestClient) -> None:
-	asset_id = asset_store.register_asset('/path/to/target.jpg')
+	create_session_response = test_client.post('/session', json =
+	{
+		'client_version': metadata.get('version')
+	})
+	create_session_body = create_session_response.json()
+	access_token = create_session_body.get('access_token')
+	session_id = session_manager.find_session_id(access_token)
+	target_path = get_test_example_file('target-240p.jpg')
+	asset_id = asset_store.create_asset(session_id, 'target', target_path).get('id')
 
-	select_response = test_client.put('/state?action=select&type=target', json =
+	select_response = test_client.put('/state?action=select&type=target', json=
 	{
 		'asset_id': asset_id
 	})
 
 	assert select_response.status_code == 401
 
-	create_session_response = test_client.post('/session', json =
-	{
-		'client_version': metadata.get('version')
-	})
-	create_session_body = create_session_response.json()
-
 	select_response = test_client.put('/state?action=select&type=target', json =
 	{
 		'asset_id': 'invalid'
 	}, headers =
 	{
-		'Authorization': 'Bearer ' + create_session_body.get('access_token')
+		'Authorization': 'Bearer ' + access_token
 	})
 
 	assert select_response.status_code == 404
@@ -158,9 +180,9 @@ def test_select_target_assets(test_client : TestClient) -> None:
 		'asset_id': asset_id
 	}, headers =
 	{
-		'Authorization': 'Bearer ' + create_session_body.get('access_token')
+		'Authorization': 'Bearer ' + access_token
 	})
 	select_body = select_response.json()
 
-	assert select_body.get('target_path') == '/path/to/target.jpg'
+	assert select_body.get('target_path') == target_path
 	assert select_response.status_code == 200
