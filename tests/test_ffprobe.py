79b38420bab48b8360d61c7986a57d36f099e329
henryruhs <info@henryruhs.com>
2026-01-22 23:05:04 +0100

    Revisit and cleanup ffprobe integration

--- tests/test_ffprobe.py
+++ tests/test_ffprobe.py
@@ -15,22 +15,22 @@ def before_all() -> None:
 	[
 		'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0/source.mp3'
 	])
-	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('source.mp3'), '-t', '1.9', '-ar', '48000', '-ac', '2', get_test_example_file('source.wav') ])
+	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('source.mp3'), '-t', '1.9', '-ar', '48000', '-ac', '2', get_test_example_file('test-audio-entries.wav') ])
 
 
 def test_detect_audio_sample_rate() -> None:
 	assert detect_audio_sample_rate(get_test_example_file('source.mp3')) == 44100
-	assert detect_audio_sample_rate(get_test_example_file('source.wav')) == 48000
+	assert detect_audio_sample_rate(get_test_example_file('test-audio-entries.wav')) == 48000
 	assert detect_audio_sample_rate(get_test_example_file('invalid.mp3')) is None
 
 
 def test_detect_audio_channel_total() -> None:
 	assert detect_audio_channel_total(get_test_example_file('source.mp3')) == 1
-	assert detect_audio_channel_total(get_test_example_file('source.wav')) == 2
+	assert detect_audio_channel_total(get_test_example_file('test-audio-entries.wav')) == 2
 	assert detect_audio_channel_total(get_test_example_file('invalid.mp3')) is None
 
 
 def test_detect_audio_frame_total() -> None:
 	assert detect_audio_frame_total(get_test_example_file('source.mp3')) == 167039
-	assert detect_audio_frame_total(get_test_example_file('source.wav')) == 91200
+	assert detect_audio_frame_total(get_test_example_file('test-audio-entries.wav')) == 91200
 	assert detect_audio_frame_total(get_test_example_file('invalid.mp3')) is None
