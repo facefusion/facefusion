aaebb56804a2ff766653b8410a6deb92da52362b
harisreedhar <h4harisreedhar.s.s@gmail.com>
2026-02-09 15:47:38 +0530

    make detect_disk_metrics stateless

--- facefusion/system.py
+++ facefusion/system.py
@@ -1,5 +1,7 @@
 import shutil
+from typing import List
 
+from facefusion import state_manager
 from facefusion.execution import detect_execution_devices
 from facefusion.types import DiskMetrics, Metrics
 
@@ -8,28 +10,33 @@ def get_metrics_set() -> Metrics:
 	return\
 	{
 		'execution_devices': detect_execution_devices(),
-		'disk': detect_disk_metrics()
+		'disks': detect_disk_metrics([ state_manager.get_temp_path() ])
 	}
 
 
-def detect_disk_metrics() -> DiskMetrics:
-	usage = shutil.disk_usage('.')
+def detect_disk_metrics(drive_paths : List[str]) -> List[DiskMetrics]:
+	disk_metrics : List[DiskMetrics] = []
 
-	return\
-	{
-		'total':
+	for drive_path in drive_paths:
+		disk_usage = shutil.disk_usage(drive_path)
+
+		disk_metrics.append(
 		{
-			'value': int(usage.total / (1024 * 1024 * 1024)),
-			'unit': 'GiB'
-		},
-		'free':
-		{
-			'value': int(usage.free / (1024 * 1024 * 1024)),
-			'unit': 'GiB'
-		},
-		'utilization':
-		{
-			'value': int(usage.used / usage.total * 100),
-			'unit': '%'
-		}
-	}
+			'total':
+			{
+				'value': int(disk_usage.total / (1024 * 1024 * 1024)),
+				'unit': 'GiB'
+			},
+			'free':
+			{
+				'value': int(disk_usage.free / (1024 * 1024 * 1024)),
+				'unit': 'GiB'
+			},
+			'utilization':
+			{
+				'value': int(disk_usage.used / disk_usage.total * 100),
+				'unit': '%'
+			}
+		})
+
+	return disk_metrics
