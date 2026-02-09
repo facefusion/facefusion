c527673846935518ba1034c4374b75ace2cdb67d
harisreedhar <h4harisreedhar.s.s@gmail.com>
2026-02-09 15:32:43 +0530

    add disk metric

--- facefusion/system.py
+++ facefusion/system.py
@@ -1,9 +1,35 @@
+import shutil
+
 from facefusion.execution import detect_execution_devices
-from facefusion.types import Metrics
+from facefusion.types import DiskMetrics, Metrics
 
 
 def get_metrics_set() -> Metrics:
 	return\
 	{
-		'execution_devices': detect_execution_devices()
+		'execution_devices': detect_execution_devices(),
+		'disk': detect_disk_metrics()
+	}
+
+
+def detect_disk_metrics() -> DiskMetrics:
+	usage = shutil.disk_usage('.')
+
+	return\
+	{
+		'total':
+		{
+			'value': int(usage.total / (1024 * 1024 * 1024)),
+			'unit': 'GiB'
+		},
+		'free':
+		{
+			'value': int(usage.free / (1024 * 1024 * 1024)),
+			'unit': 'GiB'
+		},
+		'utilization':
+		{
+			'value': int(usage.used / usage.total * 100),
+			'unit': '%'
+		}
 	}
