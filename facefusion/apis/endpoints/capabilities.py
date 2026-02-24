c0ce6d4d5e850c5f39bec95588e5d397c061c8f1
Henry Ruhs <info@henryruhs.com>
2026-02-24 11:35:33 +0100

    rename args store to capability store (#1048)
    * rename args store to capability store
    
    * fix lint and remove unused types

--- facefusion/apis/endpoints/capabilities.py
+++ facefusion/apis/endpoints/capabilities.py
@@ -3,7 +3,7 @@ from starlette.responses import JSONResponse
 from starlette.status import HTTP_200_OK
 
 import facefusion.choices
-from facefusion import args_store
+from facefusion import capability_store
 
 
 async def get_capabilities(request : Request) -> JSONResponse:
@@ -15,6 +15,6 @@ async def get_capabilities(request : Request) -> JSONResponse:
 			'image': facefusion.choices.image_formats,
 			'video': facefusion.choices.video_formats
 		},
-		'arguments': args_store.get_api_argument_set()
+		'arguments': capability_store.get_api_capability_set()
 	}
 	return JSONResponse(capabilities, status_code = HTTP_200_OK)
