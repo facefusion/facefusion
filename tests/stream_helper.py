d0584f8c23b1d9f8d14a7d460dffccd865f79068
harisreedhar <h4harisreedhar.s.s@gmail.com>
2026-03-03 21:29:09 +0530

    rename variables with rtc_ prefix

--- tests/stream_helper.py
+++ tests/stream_helper.py
@@ -4,14 +4,14 @@ from aiortc import RTCPeerConnection, VideoStreamTrack
 
 
 async def create_webrtc_offer() -> Dict[str, str]:
-	connection = RTCPeerConnection()
-	connection.addTrack(VideoStreamTrack())
-	offer = await connection.createOffer()
-	await connection.setLocalDescription(offer)
+	rtc_connection = RTCPeerConnection()
+	rtc_connection.addTrack(VideoStreamTrack())
+	rtc_offer = await rtc_connection.createOffer()
+	await rtc_connection.setLocalDescription(rtc_offer)
 	offer_dict =\
 	{
-		'sdp': connection.localDescription.sdp,
-		'type': connection.localDescription.type
+		'sdp': rtc_connection.localDescription.sdp,
+		'type': rtc_connection.localDescription.type
 	}
-	await connection.close()
+	await rtc_connection.close()
 	return offer_dict
