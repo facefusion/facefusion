a1ce24c0f3a9d7b4eea69bdc28c157cb99cf72c4
henryruhs <info@henryruhs.com>
2026-03-04 09:43:17 +0100

    prevent ConnectionResetError under windows

--- facefusion/uis/core.py
+++ facefusion/uis/core.py
@@ -1,3 +1,4 @@
+import asyncio
 import importlib
 import os
 import warnings
@@ -9,6 +10,7 @@ from gradio.themes import Size
 
 import facefusion.uis.overrides as uis_overrides
 from facefusion import logger, metadata, state_manager, translator
+from facefusion.common_helper import is_windows
 from facefusion.exit_helper import hard_exit
 from facefusion.filesystem import resolve_relative_path
 from facefusion.uis.types import Component, ComponentName
@@ -71,6 +73,9 @@ def register_ui_component(component_name : ComponentName, component: Component) 
 def init() -> None:
 	os.environ['GRADIO_ANALYTICS_ENABLED'] = '0'
 	os.environ['GRADIO_TEMP_DIR'] = os.path.join(state_manager.get_item('temp_path'), 'gradio')
+
+	if is_windows():
+		asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 	warnings.filterwarnings('ignore', category = UserWarning, module = 'gradio')
 	gradio.processing_utils._check_allowed = uis_overrides.mock
