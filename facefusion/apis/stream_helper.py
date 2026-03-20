facd90819628e2f1cbd6a92e9934b8f6a0739e32
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2026-03-20 18:39:19 +0530

    Dynamic bitrate for webrtc stream (#1063)
    * use custom aiortc
    
    * update naming to bitrate_

--- facefusion/apis/stream_helper.py
+++ facefusion/apis/stream_helper.py
@@ -1,7 +1,8 @@
 import asyncio
-from typing import cast
+from typing import Tuple
 
-from aiortc import MediaStreamTrack, RTCPeerConnection, VideoStreamTrack
+from aiortc import MediaStreamTrack, QueuedVideoStreamTrack, RTCPeerConnection, RTCRtpSender
+from aiortc.mediastreams import MediaStreamError
 from av import VideoFrame
 
 from facefusion.streamer import process_vision_frame
@@ -10,27 +11,34 @@ from facefusion.streamer import process_vision_frame
 def process_stream_frame(target_stream_frame : VideoFrame) -> VideoFrame:
 	target_vision_frame = target_stream_frame.to_ndarray(format = 'bgr24')
 	output_vision_frame = process_vision_frame(target_vision_frame)
-	return VideoFrame.from_ndarray(output_vision_frame, format = 'bgr24')
+	output_stream_frame = VideoFrame.from_ndarray(output_vision_frame, format = 'bgr24')
+	output_stream_frame.pts = target_stream_frame.pts
+	output_stream_frame.time_base = target_stream_frame.time_base
+	return output_stream_frame
 
 
-def create_output_track(target_track : MediaStreamTrack) -> VideoStreamTrack:
-	output_track = VideoStreamTrack()
-
-	async def read_stream_frame() -> VideoFrame:
-		target_stream_frame = cast(VideoFrame, await target_track.recv())
-		output_stream_frame = await asyncio.get_running_loop().run_in_executor(None, process_stream_frame, target_stream_frame)
-		output_stream_frame.pts = target_stream_frame.pts
-		output_stream_frame.time_base = target_stream_frame.time_base
-		return output_stream_frame
-
-	output_track.recv = read_stream_frame
-	return output_track
+def create_output_track(rtc_connection : RTCPeerConnection, buffer_size : int) -> Tuple[QueuedVideoStreamTrack, RTCRtpSender]:
+	output_track = QueuedVideoStreamTrack(buffer_size = buffer_size)
+	sender = rtc_connection.addTrack(output_track)
+	return output_track, sender
 
 
-def on_video_track(rtc_connection : RTCPeerConnection, target_track : MediaStreamTrack) -> None:
+async def process_and_enqueue(target_track : MediaStreamTrack, output_track : QueuedVideoStreamTrack) -> None:
+	loop = asyncio.get_running_loop()
+
+	while True:
+		try:
+			target_stream_frame = await target_track.recv()
+		except MediaStreamError:
+			pass
+
+		output_stream_frame = await loop.run_in_executor(None, process_stream_frame, target_stream_frame) #type:ignore[arg-type]
+		await output_track.put(output_stream_frame)
+
+
+def on_video_track(rtc_connection : RTCPeerConnection, output_track : QueuedVideoStreamTrack, target_track : MediaStreamTrack) -> None:
 	if target_track.kind == 'audio':
 		rtc_connection.addTrack(target_track)
 
 	if target_track.kind == 'video':
-		output_track = create_output_track(target_track)
-		rtc_connection.addTrack(output_track)
+		asyncio.create_task(process_and_enqueue(target_track, output_track))
