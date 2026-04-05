7b1cdff8de19129fa4e73057e3e5a553a718c017
henryruhs <info@henryruhs.com>
2026-04-05 10:14:49 +0200

    clean testing

--- tests/test_ffprobe.py
+++ tests/test_ffprobe.py
@@ -16,37 +16,37 @@ def before_all() -> None:
 		'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0/source.mp3',
 		'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0/target-240p.mp4'
 	])
-	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('source.mp3'), '-t', '1.9', '-ar', '48000', '-ac', '2', get_test_example_file('test-audio-entries.wav') ])
+	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('source.mp3'), '-t', '1.9', '-ar', '48000', '-ac', '2', get_test_example_file('source-48000khz-2ch.wav') ])
+	subprocess.run([ 'ffmpeg', '-i', get_test_example_file('target-240p.mp4'), '-t', '1', get_test_example_file('target-240p-1s.mov') ])
 
 
-def test_parse_audio_codec() -> None:
-	assert probe_entries(get_test_example_file('source.mp3'), ['codec_name']).get('codec_name') == 'mp3'
-	assert probe_entries(get_test_example_file('test-audio-entries.wav'), ['codec_name']).get('codec_name') == 'pcm_s16le'
-	assert probe_entries(get_test_example_file('invalid.mp3'), ['codec_name']).get('codec_name') is None
-
-
-def test_probe_audio() -> None:
+def test_extract_audio_metadata() -> None:
 	audio_metadata = extract_audio_metadata(get_test_example_file('source.mp3'))
+
 	assert audio_metadata.get('sample_rate') == 44100
 	assert audio_metadata.get('channel_total') == 1
 	assert audio_metadata.get('frame_total') == 167039
 	assert audio_metadata.get('bit_rate') == 128000
 
-	audio_metadata = extract_audio_metadata(get_test_example_file('test-audio-entries.wav'))
+	audio_metadata = extract_audio_metadata(get_test_example_file('source-48000khz-2ch.wav'))
+
 	assert audio_metadata.get('sample_rate') == 48000
 	assert audio_metadata.get('channel_total') == 2
 	assert audio_metadata.get('frame_total') == 91200
 	assert audio_metadata.get('bit_rate') == 1536000
 
 
-def test_parse_video_codec() -> None:
-	assert probe_entries(get_test_example_file('target-240p.mp4'), ['codec_name']).get('codec_name') == 'h264'
-	assert probe_entries(get_test_example_file('invalid.mp4'), ['codec_name']).get('codec_name') is None
-
-
-def test_probe_video() -> None:
+def test_xtract_video_metadata() -> None:
 	video_metadata = extract_video_metadata(get_test_example_file('target-240p.mp4'))
+
 	assert video_metadata.get('fps') == 25.0
 	assert video_metadata.get('duration') == 10.8
 	assert video_metadata.get('resolution') == (426, 226)
 	assert video_metadata.get('bit_rate') == 138754
+
+	video_metadata = extract_video_metadata(get_test_example_file('target-240p-1s.mov'))
+
+	assert video_metadata.get('fps') == 25.0
+	assert video_metadata.get('duration') == 1.08
+	assert video_metadata.get('resolution') == (426, 226)
+	assert video_metadata.get('bit_rate') == 165600
