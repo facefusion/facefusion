a6224caf2eec69e3b766092a2aecb64e1a36a9bd
henryruhs <info@henryruhs.com>
2026-05-10 14:02:21 +0200

    disable tests to see what happens

--- tests/test_library_datachannel.py
+++ tests/test_library_datachannel.py
@@ -3,6 +3,7 @@ import ctypes
 import pytest
 
 from facefusion import state_manager
+from facefusion.common_helper import is_windows
 from facefusion.libraries import datachannel as datachannel_module
 
 
@@ -12,5 +13,7 @@ def before_all() -> None:
 	datachannel_module.pre_check()
 
 
+# TODO: add support for Windows
+@pytest.mark.skipif(is_windows(), reason = 'not supported on Windows')
 def test_create_static_library() -> None:
 	assert isinstance(datachannel_module.create_static_library(), ctypes.CDLL)
--- tests/test_library_opus.py
+++ tests/test_library_opus.py
@@ -1,12 +1,19 @@
 import ctypes
 
+import pytest
+
+from facefusion.common_helper import is_windows
 from facefusion.libraries import opus as opus_module
 
 
+# TODO: add support for Windows
+@pytest.mark.skipif(is_windows(), reason = 'not supported on Windows')
 def test_create_static_library() -> None:
 	assert isinstance(opus_module.create_static_library(), ctypes.CDLL)
 
 
+# TODO: add support for Windows
+@pytest.mark.skipif(is_windows(), reason = 'not supported on Windows')
 def test_create_opus_encoder() -> None:
 	opus_library = opus_module.create_static_library()
 
