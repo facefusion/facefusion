1491b7496ca492ff516697af418d649b64bbfe25
henryruhs <info@henryruhs.com>
2026-05-10 13:06:59 +0200

    fix testing

--- tests/test_library_datachannel.py
+++ tests/test_library_datachannel.py
@@ -1,6 +1,15 @@
 import ctypes
 
+import pytest
+
+from facefusion import state_manager
 from facefusion.libraries import datachannel as datachannel_module
+
+
+@pytest.fixture(scope = 'module', autouse = True)
+def before_all() -> None:
+	state_manager.init_item('download_providers', [ 'github', 'huggingface' ])
+	datachannel_module.pre_check()
 
 
 def test_create_static_library() -> None:
--- tests/test_rtc_store.py
+++ tests/test_rtc_store.py
@@ -1,10 +1,12 @@
 import pytest
 
+from facefusion import state_manager
 from facefusion.libraries import datachannel as datachannel_module
 
 
-@pytest.fixture(scope = 'module')
+@pytest.fixture(scope = 'module', autouse = True)
 def before_all() -> None:
+	state_manager.init_item('download_providers', [ 'github', 'huggingface' ])
 	datachannel_module.pre_check()
 
 
