ce943ae939488c36d8560773df913245f0a02129
henryruhs <info@henryruhs.com>
2026-05-10 17:20:09 +0200

    debug ci

--- tests/test_api_stream.py
+++ tests/test_api_stream.py
@@ -9,8 +9,8 @@ from starlette.testclient import TestClient
 
 from facefusion import metadata, session_manager, state_manager
 from facefusion.apis import asset_store
-from facefusion.common_helper import is_macos, is_windows
 from facefusion.apis.core import create_api
+from facefusion.common_helper import is_macos, is_windows
 from facefusion.core import common_pre_check, processors_pre_check
 from facefusion.download import conditional_download
 from facefusion.libraries import datachannel as datachannel_module
