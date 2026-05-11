c4ac5eb7a31018c8a0526db18e5c392e6e5ca1dd
harisreedhar <h4harisreedhar.s.s@gmail.com>
2026-05-11 14:06:39 +0530

    improve test

--- tests/test_video_encoder.py
+++ tests/test_video_encoder.py
@@ -15,9 +15,10 @@ def before_all() -> None:
 def test_encode_vpx() -> None:
 	codec_context = create_vpx_encoder(320, 240, 1000)
 	yuv_buffer = bytes(320 * 240 * 3 // 2)
+	invalid_yuv_buffer = bytes(640 * 480 * 3 // 2)
 
 	assert isinstance(encode_vpx(codec_context, yuv_buffer, 320, 240, 0, 0), bytes)
 	assert isinstance(encode_vpx(codec_context, yuv_buffer, 320, 240, 2, 0), bytes)
-	assert encode_vpx(codec_context, yuv_buffer, 320, 240, 1, 0)
-	assert encode_vpx(codec_context, yuv_buffer, 320, 240, 3, 1)
-	assert encode_vpx(codec_context, bytes(640 * 480 * 3 // 2), 640, 480, 0, 0) == b''
+	assert encode_vpx(codec_context, yuv_buffer, 320, 240, 1, 0)[0] & 1 == 1
+	assert encode_vpx(codec_context, yuv_buffer, 320, 240, 3, 1)[3:].startswith(b'\x9d\x01\x2a')
+	assert encode_vpx(codec_context, invalid_yuv_buffer, 640, 480, 0, 0) == b''
