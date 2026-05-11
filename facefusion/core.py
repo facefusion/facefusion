00764df1c7cc88d4214ceecc3181995758889bd7
henryruhs <info@henryruhs.com>
2026-05-11 02:37:43 +0200

    switch to self hosted libraries

--- facefusion/core.py
+++ facefusion/core.py
@@ -12,9 +12,9 @@ from facefusion.apis.core import create_api
 from facefusion.args_helper import apply_args
 from facefusion.download import conditional_download_hashes, conditional_download_sources
 from facefusion.exit_helper import hard_exit, signal_exit
-from facefusion.libraries import datachannel as datachannel_module, opus as opus_module, vpx as vpx_module
 from facefusion.filesystem import get_file_extension, has_audio, has_image, has_video
 from facefusion.filesystem import get_file_name, resolve_file_paths, resolve_file_pattern
+from facefusion.libraries import datachannel as datachannel_module, opus as opus_module, vpx as vpx_module
 from facefusion.jobs import job_helper, job_manager, job_runner
 from facefusion.jobs.job_list import compose_job_list
 from facefusion.processors.core import get_processors_modules
