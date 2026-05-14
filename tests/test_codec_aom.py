a09703488927cfb2aad43ba95b199d0566ccaa72
henryruhs <info@henryruhs.com>
2026-05-14 19:32:14 +0200

    skip test for macos

--- tests/test_codec_aom.py
+++ tests/test_codec_aom.py
@@ -37,7 +37,7 @@ def test_encode_aom_buffer() -> None:
 		assert create_hash(encode_aom_buffer(aom_encoder, video_buffer, video_resolution, 3)) == '3ab6cc31'
 
 	if is_macos():
-		assert encode_aom_buffer(aom_encoder, video_buffer, video_resolution, 3)
+		pytest.skip()
 
 
 def test_destroy_aom_encoder() -> None:
