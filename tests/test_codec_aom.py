c6e7f68439c6c2f6bedf91b7c58673927533e9c7
henryruhs <info@henryruhs.com>
2026-05-14 12:38:52 +0200

    fix test for macos

--- tests/test_codec_aom.py
+++ tests/test_codec_aom.py
@@ -37,7 +37,7 @@ def test_encode_aom_buffer() -> None:
 		assert create_hash(encode_aom_buffer(aom_encoder, video_buffer, video_resolution, 3)) == '4b621fb8'
 
 	if is_macos():
-		assert encode_aom_buffer(aom_encoder, video_buffer, video_resolution, 3)
+		assert create_hash(encode_aom_buffer(aom_encoder, video_buffer, video_resolution, 3)) == '6c5f613c'
 
 
 def test_destroy_aom_encoder() -> None:
