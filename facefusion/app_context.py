57189c638e6c5abc089a70aaf741d18430265ce1
henryruhs <info@henryruhs.com>
2026-06-09 22:30:37 +0200

    optimize app context detection

--- facefusion/app_context.py
+++ facefusion/app_context.py
@@ -5,12 +5,14 @@ from facefusion.types import AppContext
 
 
 def detect_app_context() -> AppContext:
+	jobs_path = os.path.join('facefusion', 'jobs')
+	apis_path = os.path.join('facefusion', 'apis')
 	frame = sys._getframe(1)
 
 	while frame:
-		if os.path.join('facefusion', 'jobs') in frame.f_code.co_filename:
+		if jobs_path in frame.f_code.co_filename:
 			return 'cli'
-		if os.path.join('facefusion', 'apis') in frame.f_code.co_filename:
+		if apis_path in frame.f_code.co_filename:
 			return 'api'
 		frame = frame.f_back
 	return 'cli'
