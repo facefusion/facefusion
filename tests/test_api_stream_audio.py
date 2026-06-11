a391e8465741392e44b1a26b485efa1b37ff0bc6
henryruhs <info@henryruhs.com>
2026-06-11 22:29:04 +0200

    fix test

--- tests/test_api_stream_audio.py
+++ tests/test_api_stream_audio.py
@@ -2,7 +2,7 @@ import ctypes
 import threading
 from functools import partial
 from queue import Queue
-from typing import Callable, Tuple
+from typing import Tuple
 from unittest.mock import MagicMock, patch
 
 import numpy
@@ -14,7 +14,7 @@ from facefusion.download import conditional_download
 from facefusion.ffmpeg import read_audio_buffer
 from facefusion.hash_helper import create_hash
 from facefusion.libraries import datachannel as datachannel_module, opus as opus_module
-from facefusion.types import AudioCodec, AudioFrame, RtcPeer, RtcPeerAudio
+from facefusion.types import AudioCodec, AudioFrame, Buffer, FrameHandler, RtcPeer, RtcPeerAudio
 from .assert_helper import get_test_example_file, get_test_examples_directory
 
 
@@ -37,8 +37,11 @@ def before_each() -> None:
 	rtc_store.clear()
 
 
-def set_ready_event(ready_event : threading.Event, track : int, close_callback : Callable[[int, ctypes.c_void_p], None]) -> None:
+def dispatch_frame(buffer : Buffer, track : int, frame_handler : FrameHandler) -> threading.Event:
+	frame_handler(buffer, 0)
+	ready_event = threading.Event()
 	ready_event.set()
+	return ready_event
 
 
 def test_run_audio_encode_loop() -> None:
@@ -48,6 +51,12 @@ def test_run_audio_encode_loop() -> None:
 	rtc_peer : RtcPeer =\
 	{
 		'peer_connection': peer_connection,
+		'audio':
+		{
+			'sender_track': 0,
+			'receiver_track': 0,
+			'codec': 'opus'
+		},
 		'video':
 		{
 			'sender_track': 0,
@@ -82,11 +91,7 @@ def test_receive_audio_frames(audio_codec : AudioCodec) -> None:
 	audio_frame = numpy.frombuffer(audio_buffer, dtype = numpy.int16).astype(numpy.float32) / 32768.0
 	audio_queue : Queue[Tuple[float, AudioFrame]] = Queue(maxsize = 300)
 
-	datachannel_mock = MagicMock()
-	ready_event = threading.Event()
-	datachannel_mock.rtcSetClosedCallback.side_effect = partial(set_ready_event, ready_event)
-
-	with patch('facefusion.libraries.datachannel.create_static_library', return_value = datachannel_mock):
+	with patch('facefusion.apis.stream_audio.create_receive_event', side_effect = partial(dispatch_frame, bytes([ 0 ]))):
 		with patch('facefusion.apis.stream_audio.decode_audio_frame', return_value = audio_frame.tobytes()):
 			rtc_peer_audio : RtcPeerAudio =\
 			{
@@ -94,12 +99,7 @@ def test_receive_audio_frames(audio_codec : AudioCodec) -> None:
 				'receiver_track': 0,
 				'codec': audio_codec
 			}
-			audio_receiver_thread = threading.Thread(target = receive_audio_frames, args = (rtc_peer_audio, audio_queue), daemon = True)
-			audio_receiver_thread.start()
-			ready_event.wait(timeout = 5.0)
-			datachannel_mock.rtcSetFrameCallback.call_args[0][1](0, bytes([ 0 ]), 1, None, None)
-			datachannel_mock.rtcSetClosedCallback.call_args[0][1](0, None)
-			audio_receiver_thread.join(timeout = 5.0)
+			receive_audio_frames(rtc_peer_audio, audio_queue)
 
 	_, temp_audio_frame = audio_queue.get_nowait()
 
