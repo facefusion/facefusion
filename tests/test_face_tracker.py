57b9445f38cef3ca77261d1196bc172dcddda249
harisreedhar <h4harisreedhar.s.s@gmail.com>
2026-06-17 14:14:08 +0530

    fix spaces

--- tests/test_face_tracker.py
+++ tests/test_face_tracker.py
@@ -5,19 +5,19 @@ from .helper import create_face_from_bounding_box
 def test_match_face_track() -> None:
 	face_track =\
 	{
-		0 : create_face_from_bounding_box([10, 10, 50, 50])
+		0 : create_face_from_bounding_box([ 10, 10, 50, 50 ])
 	}
 
-	assert match_face_track([ face_track ], create_face_from_bounding_box([12, 12, 52, 52]), 1, 0.3) is face_track
-	assert match_face_track([ face_track ], create_face_from_bounding_box([200, 200, 240, 240]), 1, 0.3) == {}
-	assert match_face_track([ face_track ], create_face_from_bounding_box([12, 12, 52, 52]), 0, 0.3) == {}
+	assert match_face_track([ face_track ], create_face_from_bounding_box([ 12, 12, 52, 52 ]), 1, 0.3) is face_track
+	assert match_face_track([ face_track ], create_face_from_bounding_box([ 200, 200, 240, 240 ]), 1, 0.3) == {}
+	assert match_face_track([ face_track ], create_face_from_bounding_box([ 12, 12, 52, 52 ]), 0, 0.3) == {}
 
 
 def test_get_anchor_indices() -> None:
 	face_track =\
 	{
-		2 : create_face_from_bounding_box([0, 0, 10, 10]),
-		8 : create_face_from_bounding_box([0, 0, 10, 10])
+		2 : create_face_from_bounding_box([ 0, 0, 10, 10 ]),
+		8 : create_face_from_bounding_box([ 0, 0, 10, 10 ])
 	}
 
 	assert get_anchor_indices(face_track, 5) == (2, 8)
@@ -26,9 +26,9 @@ def test_get_anchor_indices() -> None:
 
 
 def test_resolve_track_face() -> None:
-	face_before = create_face_from_bounding_box([0, 0, 10, 10])
-	face_target = create_face_from_bounding_box([40, 40, 50, 50])
-	face_after = create_face_from_bounding_box([80, 80, 90, 90])
+	face_before = create_face_from_bounding_box([ 0, 0, 10, 10 ])
+	face_target = create_face_from_bounding_box([ 40, 40, 50, 50 ])
+	face_after = create_face_from_bounding_box([ 80, 80, 90, 90 ])
 
 	assert resolve_track_face(
 	{
