218784dd3700ad01d1983bd16a68290617ffb8c5
henryruhs <info@henryruhs.com>
2026-06-24 08:22:19 +0200

    fix audio offset

--- facefusion/uis/components/preview.py
+++ facefusion/uis/components/preview.py
@@ -191,7 +191,7 @@ def update_preview_image(preview_mode : PreviewMode, preview_resolution : str, f
 	source_voice_frame = create_empty_audio_frame()
 
 	if source_audio_path and state_manager.get_item('output_video_fps') and state_manager.get_item('reference_frame_number'):
-		audio_frame_number = state_manager.get_item('reference_frame_number')
+		audio_frame_number = frame_number
 		if state_manager.get_item('trim_frame_start'):
 			audio_frame_number -= state_manager.get_item('trim_frame_start')
 		temp_voice_frame = get_voice_frame(source_audio_path, state_manager.get_item('output_video_fps'), audio_frame_number)
