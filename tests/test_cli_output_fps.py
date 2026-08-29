958c5024c2cb36213fc39f8057c5664fcec13aa2
henryruhs <info@henryruhs.com>
2026-08-29 10:19:01 +0200

    significant improve the test

--- tests/test_cli_output_fps.py
+++ tests/test_cli_output_fps.py
@@ -21,20 +21,21 @@ def before_all() -> None:
 		'https://github.com/facefusion/facefusion-assets/releases/download/examples-3.0.0/target-240p.mp4'
 	])
 
-	ffmpeg.run_ffmpeg(
-		ffmpeg_builder.chain(
-			ffmpeg_builder.set_input(get_test_example_file('target-240p.mp4')),
-			ffmpeg_builder.select_frame_range(0, 40, 6.25),
-			ffmpeg_builder.set_video_encoder('libx264'),
-			ffmpeg_builder.enforce_pixel_format('yuv420p'),
-			[
-				'-crf',
-				'0',
-				'-an'
-			],
-			ffmpeg_builder.force_output(get_test_example_file('target-240p-6.25fps.mp4'))
+	for output_video_fps in [ 30, 60 ]:
+		ffmpeg.run_ffmpeg(
+			ffmpeg_builder.chain(
+				ffmpeg_builder.set_input(get_test_example_file('target-240p.mp4')),
+				ffmpeg_builder.select_frame_range(0, 30, output_video_fps),
+				ffmpeg_builder.set_video_encoder('libx264'),
+				ffmpeg_builder.enforce_pixel_format('yuv420p'),
+				[
+					'-crf',
+					'0',
+					'-an'
+				],
+				ffmpeg_builder.force_output(get_test_example_file('target-240p-' + str(output_video_fps) + 'fps.mp4'))
+			)
 		)
-	)
 
 
 @pytest.fixture(scope = 'function', autouse = True)
@@ -44,22 +45,28 @@ def before_each() -> None:
 	prepare_test_output_directory()
 
 
-@pytest.mark.parametrize('workflow_strategy, output_video_fps, output_video_frame_total',
+@pytest.mark.parametrize('workflow_strategy, output_video_fps, trim_frame_end, output_video_frame_total',
 [
-	('disk', 6.25, 10),
-	('memory', 6.25, 10)
+	('disk', 30, 30, 36),
+	('memory', 30, 30, 36),
+	('disk', 60, 30, 72),
+	('memory', 60, 30, 72)
 ])
-def test_output_video_fps(workflow_strategy : WorkflowStrategy, output_video_fps : Fps, output_video_frame_total : int) -> None:
-	output_file_path = get_test_output_file('test-output-video-fps-' + workflow_strategy + '-' + str(output_video_fps) + '.mp4')
-	resample_file_path = get_test_output_file('test-output-video-fps-resample-' + workflow_strategy + '-' + str(output_video_fps) + '.mp4')
-	commands = [ sys.executable, 'facefusion.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--processors', 'face_swapper', '--execution-providers', 'cpu', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.mp4'), '-o', output_file_path, '--trim-frame-end', '40', '--workflow-strategy', workflow_strategy, '--output-video-fps', str(output_video_fps) ]
-	resample_commands = [ sys.executable, 'facefusion.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--processors', 'face_swapper', '--execution-providers', 'cpu', '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p-' + str(output_video_fps) + 'fps.mp4'), '-o', resample_file_path, '--workflow-strategy', workflow_strategy ]
+def test_output_video_fps(workflow_strategy : WorkflowStrategy, output_video_fps : Fps, trim_frame_end : int, output_video_frame_total : int) -> None:
+	actual_file_path = get_test_output_file('test-output-video-fps-actual-' + workflow_strategy + '-' + str(output_video_fps) + '.mp4')
+	expect_file_path = get_test_output_file('test-output-video-fps-expect-' + workflow_strategy + '-' + str(output_video_fps) + '.mp4')
+	actual_commands = [ sys.executable, 'facefusion.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--workflow-strategy', workflow_strategy, '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p.mp4'), '-o', actual_file_path, '--trim-frame-end', str(trim_frame_end), '--output-video-fps', str(output_video_fps) ]
+	expect_commands = [ sys.executable, 'facefusion.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--workflow-strategy', workflow_strategy, '-s', get_test_example_file('source.jpg'), '-t', get_test_example_file('target-240p-' + str(output_video_fps) + 'fps.mp4'), '-o', expect_file_path ]
 
-	assert subprocess.run(commands).returncode == 0
-	assert subprocess.run(resample_commands).returncode == 0
-	assert detect_video_fps(output_file_path) == output_video_fps
-	assert count_video_frame_total(output_file_path) == output_video_frame_total
-	assert count_video_frame_total(resample_file_path) == output_video_frame_total
+	assert subprocess.run(actual_commands).returncode == 0
+	assert subprocess.run(expect_commands).returncode == 0
+
+	assert detect_video_fps(actual_file_path) == output_video_fps
+	assert count_video_frame_total(actual_file_path) == output_video_frame_total
+	assert count_video_frame_total(expect_file_path) == output_video_frame_total
 
 	for frame_number in range(output_video_frame_total):
-		assert numpy.array_equal(read_video_frame(output_file_path, frame_number), read_video_frame(resample_file_path, frame_number)) is True
+		actual_vision_frame = read_video_frame(actual_file_path, frame_number)
+		expect_vision_frame = read_video_frame(expect_file_path, frame_number)
+
+		assert numpy.array_equal(actual_vision_frame, expect_vision_frame) is True
