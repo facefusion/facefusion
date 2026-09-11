d50437ae04d3918c922afe4c3cbf31c87897d68f
henryruhs <info@henryruhs.com>
2026-09-11 14:03:18 +0200

    stateless test helpers

--- tests/assert_helper.py
+++ tests/assert_helper.py
@@ -1,8 +1,8 @@
 import os
 import tempfile
 
-from facefusion import state_manager
 from facefusion.filesystem import are_images, create_directory, is_directory, is_file, remove_directory, resolve_file_paths
+from facefusion.session_context import resolve_local_id
 from facefusion.types import JobStatus
 
 
@@ -11,8 +11,7 @@ def is_test_job_file(file_path : str, job_status : JobStatus) -> bool:
 
 
 def get_test_job_file(file_path : str, job_status : JobStatus) -> str:
-	jobs_path = state_manager.get_jobs_path()
-	return os.path.join(jobs_path, job_status, file_path)
+	return os.path.join(get_test_jobs_directory(), resolve_local_id(), job_status, file_path)
 
 
 def get_test_jobs_directory() -> str:
