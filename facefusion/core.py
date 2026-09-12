1052b6f5328f246d81928eeb95b0667aa80c101f
Henry Ruhs <info@henryruhs.com>
2026-09-12 16:32:46 +0200

    convert face store (#1241)

--- facefusion/core.py
+++ facefusion/core.py
@@ -8,7 +8,7 @@ from time import time
 import uvicorn
 
 import facefusion.apis.core
-from facefusion import args_helper, benchmarker, cli_helper, content_analyser, content_store, hash_helper, inference_manager, logger, process_manager, session_manager, state_manager, translator, video_manager
+from facefusion import args_helper, benchmarker, cli_helper, content_analyser, content_store, face_store, hash_helper, inference_manager, logger, process_manager, session_manager, state_manager, translator, video_manager
 from facefusion.args_helper import apply_args
 from facefusion.download import conditional_download_hashes, conditional_download_sources
 from facefusion.exit_helper import hard_exit, signal_exit
@@ -39,6 +39,7 @@ def cli() -> None:
 				logger.init(state_manager.get_item('log_level'))
 
 				content_store.init()
+				face_store.init()
 				inference_manager.init()
 				process_manager.init()
 				video_manager.init()
--- facefusion/face_store.py
+++ facefusion/face_store.py
@@ -1,41 +1,58 @@
 import threading
 from typing import List, Optional
 
+from facefusion import store_creator
 from facefusion.hash_helper import create_hash
-from facefusion.types import Face, FaceStore, VisionFrame
+from facefusion.session_manager import resolve_owner_id
+from facefusion.types import Face, Store, VisionFrame
 from facefusion.vision import is_vision_frame
 
-FACE_STORE : FaceStore = {}
+FACE_STORE : Store = store_creator.create_store({})
+
+
+def init() -> None:
+	owner_id = resolve_owner_id()
+	store_creator.init_content(FACE_STORE, owner_id)
 
 
 def get_faces(vision_frame : VisionFrame) -> Optional[List[Face]]:
+	owner_id = resolve_owner_id()
+	face_store = store_creator.get_content(FACE_STORE, owner_id)
+
 	if is_vision_frame(vision_frame):
 		vision_hash = create_hash(vision_frame.tobytes())
 
-		if FACE_STORE.get(vision_hash):
-			return FACE_STORE.get(vision_hash).get('faces')
+		if face_store.get(vision_hash):
+			return face_store.get(vision_hash).get('faces')
 
 	return None
 
 
 def set_faces(vision_frame : VisionFrame, faces : List[Face]) -> None:
+	owner_id = resolve_owner_id()
+	face_store = store_creator.get_content(FACE_STORE, owner_id)
+
 	if is_vision_frame(vision_frame):
 		vision_hash = create_hash(vision_frame.tobytes())
-		FACE_STORE.setdefault(vision_hash,
+		face_store.setdefault(vision_hash,
 		{
 			'lock': threading.Lock()
 		})['faces'] = faces
 
 
 def resolve_lock(vision_frame : VisionFrame) -> threading.Lock:
+	owner_id = resolve_owner_id()
+	face_store = store_creator.get_content(FACE_STORE, owner_id)
+
 	if is_vision_frame(vision_frame):
 		vision_hash = create_hash(vision_frame.tobytes())
-		return FACE_STORE.setdefault(vision_hash,
+		return face_store.setdefault(vision_hash,
 		{
 			'lock': threading.Lock()
 		}).get('lock')
 	return threading.Lock()
 
 
-def clear_faces() -> None:
-	FACE_STORE.clear()
+def clear() -> None:
+	owner_id = resolve_owner_id()
+	store_creator.init_content(FACE_STORE, owner_id)
